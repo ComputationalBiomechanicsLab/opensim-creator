cad3e240518a1d077fcd64432372b9ebe20146ec
Adam Kewley <contact@adamkewley.com>
2021-01-22 09:01:04 +0000

    Added first steps of Windows e2e build

---
+++ scripts/windows_e2e-build.bat
@@ -0,0 +1,22 @@
+REM Windows: end-2-end build
+REM
+REM     - this should build on a clean Windows 2019 Server PC
+
+REM checkout OpenSim 4.1
+git clone --single-branch --branch 4.1 --depth=1 https://github.com/opensim-org/opensim-core
+
+REM build OpenSim dependencies
+dir .
+mkdir opensim-dependencies-build
+cd opensim-dependencies-build
+cmake ../opensim-core/dependencies -G"Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=../dependencies-install
+cmake --build . --config Release
+dir .
+cd ..
+
+REM build OpenSim
+mkdir opensim-build
+cd opensim-build
+cmake ../opensim-core -G"Visual Studio 15 2017" -A x64 -DOPENSIM_DEPENDENCIES_DIR=../dependencies-install -DBUILD_JAVA_WRAPPING=OFF -DCMAKE_INSTALL_PREFIX=../opensim-install
+cmake --build . --config Release --target install
+cd ..
