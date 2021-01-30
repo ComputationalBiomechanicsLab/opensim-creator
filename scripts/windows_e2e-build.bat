6b207743185e4ecabddf02c0995f8afdb32e7d2c
Adam Kewley <contact@adamkewley.com>
2021-01-30 14:14:04 +0000

    Added error checking to windows build script (should fail)

--- scripts/windows_e2e-build.bat
+++ scripts/windows_e2e-build.bat
@@ -3,26 +3,28 @@ REM
 REM     - this should build on a clean Windows 2019 Server PC
 
 REM checkout OpenSim 4.1
-git clone --single-branch --branch master --depth=1 https://github.com/opensim-org/opensim-core
+git clone --single-branch --branch master --depth=1 https://github.com/opensim-org/opensim-core || exit /b
 
 REM build OpenSim dependencies
 dir .
 mkdir opensim-dependencies-build
 cd opensim-dependencies-build
-cmake ../opensim-core/dependencies -G"Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=../dependencies-install
-cmake --build . --config Release
+cmake ../opensim-core/dependencies -G"Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=../dependencies-install || exit /b
+cmake --build . --config Release || exit /b
 dir .
 cd ..
 
 REM build OpenSim
 mkdir opensim-build
 cd opensim-build
-cmake ../opensim-core -G"Visual Studio 16 2019" -A x64 -DOPENSIM_DEPENDENCIES_DIR=../dependencies-install -DBUILD_JAVA_WRAPPING=OFF -DCMAKE_INSTALL_PREFIX=../opensim-install
-cmake --build . --config Release --target install
+cmake ../opensim-core -G"Visual Studio 16 2019" -A x64 -DOPENSIM_DEPENDENCIES_DIR=../dependencies-install -DBUILD_JAVA_WRAPPING=OFF -DCMAKE_INSTALL_PREFIX=../opensim-install || exit /b
+cmake --build . --config Release --target install || exit /b
 cd ..
 
 mkdir osmv-build
 cd osmv-build
-cmake ../ -G"Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH=../opensim-install/sdk
-cmake --build . --config Release --target osmv
+cmake ../ -G"Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH=../opensim-install/sdk || exit /b
+cmake --build . --config Release --target osmv || exit /b
 cd ..
+
+if %errorlevel% neq 0 exit /b %errorlevel%
