18d53b020b42c2ea7e359b721dd5522efa722396
Adam Kewley <contact@adamkewley.com>
2021-02-03 16:21:03 +0100

    Fleshed out mac build script

--- scripts/mac_catalina_10-15_build.sh
+++ scripts/mac_catalina_10-15_build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+set -xeuo pipefail
+
+num_workers=$(sysctl -n hw.physicalcpu)
+skip_opensim_download=${skip_opensim_download:0}
+
 # (if building OpenSim): get OpenSim 4.1 source
-git clone --single-branch --branch 4.1 --depth=1 https://github.com/opensim-org/opensim-core
+if [[ "${skip_opensim_download}" -eq "0" ]]; then
+    git clone --single-branch --branch 4.1 --depth=1 https://github.com/opensim-org/opensim-core
+fi
 
 # ----- build: build OpenSim (optional) then build osmv ----- #
 
@@ -9,18 +16,18 @@ git clone --single-branch --branch 4.1 --depth=1 https://github.com/opensim-org/
 mkdir -p opensim-dependencies-build/
 cd opensim-dependencies-build/
 cmake ../opensim-core/dependencies -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../opensim-dependencies-install
-cmake --build . -- -j 4
+cmake --build . -- -j ${num_workers}
 cd -
 
 # (if building OpenSim): build OpenSim
 mkdir -p opensim-build/
 cd opensim-build/
 cmake ../opensim-core/ -DOPENSIM_DEPENDENCIES_DIR=../opensim-dependencies-install/ -DCMAKE_INSTALL_PREFIX=../opensim-install/ -DBUILD_JAVA_WRAPPING=OFF -DCMAKE_BUILD_TYPE=Release
-cmake --build . --target install -- -j 4
+cmake --build . --target install -- -j ${num_workers}
 cd -
 
-mkdir osmv-build/
+mkdir -p osmv-build/
 cd osmv-build/
-cmake ../osmv -DCMAKE_PREFIX_PATH=../opensim-install/lib/cmake -DCMAKE_BUILD_TYPE=Release
-cmake --build . --target osmv -- -j 4
+cmake .. -DCMAKE_PREFIX_PATH=${PWD}/../opensim-install -DCMAKE_BUILD_TYPE=Release
+cmake --build . --target osmv -- -j ${num_workers}
 
