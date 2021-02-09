2af17d35dc383fb5e0fbc0e521d001450c9b6a58
Adam Kewley <contact@adamkewley.com>
2021-02-09 10:38:19 +0000

    Improved hierarchy viewer and added 'sockets' to selection panel

--- src/show_model_screen.cpp
+++ src/show_model_screen.cpp
@@ -1171,8 +1171,84 @@ namespace osmv {
         }
 
         void draw_hierarchy_tab() {
-            for (OpenSim::Component const& c : model->getComponentList()) {
-                ImGui::Text("%s", c.getAbsolutePath().toString().c_str());
+            std::string output_str;
+            OpenSim::Component const* root = &model->getRoot();
+            std::array<OpenSim::Component const*, 32> prev_path;
+            size_t prev_path_sz = 0;
+            bool header_showing = true;
+            for (OpenSim::Component const& cr : model->getComponentList()) {
+
+                std::array<OpenSim::Component const*, 32> cur_path;
+                size_t i = 0;
+
+                {
+                    // push each element in the tree into a stack (child --> parent)
+                    OpenSim::Component const* c = &cr;
+                    while (c != root) {
+                        assert(i < cur_path.size());
+                        cur_path[i++] = c;
+                        c = &c->getOwner();
+                    }
+
+                    // reverse the stack to yield a linear sequence (parent --> child)
+                    std::reverse(cur_path.begin(), cur_path.begin() + i);
+                }
+
+                size_t indent = 0;
+                {
+                    // figure out common subpath between this path and the previous one
+                    size_t len = std::min(prev_path_sz, i);
+                    while (indent < len and prev_path[indent] == cur_path[indent]) {
+                        ++indent;
+                    }
+                }
+
+                if (indent == 0) {
+                    // edge-case: first-level elements should be put into a collapsing header that
+                    //            the user can toggle
+                    header_showing = ImGui::CollapsingHeader(cur_path[indent]->getName().c_str());
+                    ++indent;
+                }
+
+                if (header_showing) {
+                    for (size_t j = indent; j < i; ++j) {
+                        // print out the not-common tail
+
+                        OpenSim::Component const* comp = cur_path[j];
+
+                        output_str.clear();
+                        for (size_t k = 0; k < indent; ++k) {
+                            output_str += "    ";
+                        }
+                        output_str += comp->getName().c_str();
+
+                        int style_pushes = 0;
+                        if (comp == renderer.hovered_component) {
+                            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4{0.5f, 0.5f, 0.0f, 1.0f});
+                            ++style_pushes;
+                        }
+                        if (comp == selected_component) {
+                            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4{1.0f, 1.0f, 0.0f, 1.0f});
+                            ++style_pushes;
+                        }
+
+                        ImGui::Text(output_str.c_str());
+                        if (ImGui::IsItemHovered()) {
+                            renderer.hovered_component = comp;
+                        }
+                        if (ImGui::IsItemClicked(ImGuiMouseButton_Right)) {
+                            selected_component = comp;
+                        }
+
+                        ImGui::PopStyleColor(style_pushes);
+                    }
+                }
+
+                {
+                    // update loop invariants
+                    std::copy(cur_path.begin(), cur_path.begin() + i, prev_path.begin());
+                    prev_path_sz = i;
+                }
             }
         }
 
@@ -1596,6 +1672,11 @@ namespace osmv {
                 ImGui::Text("%s", c.getName().c_str());
                 ImGui::NextColumn();
 
+                ImGui::Text("getAuthors()");
+                ImGui::NextColumn();
+                ImGui::Text("%s", c.getAuthors().c_str());
+                ImGui::NextColumn();
+
                 ImGui::Text("getOwner().getName()");
                 ImGui::NextColumn();
                 ImGui::Text("%s", c.getOwner().getName().c_str());
@@ -1672,6 +1753,14 @@ namespace osmv {
                 ImGui::Columns();
             }
 
+            // inputs
+            if (ImGui::CollapsingHeader("inputs")) {
+                std::vector<std::string> input_names = c.getInputNames();
+                for (std::string const& input_name : input_names) {
+                    ImGui::Text(input_name.c_str());
+                }
+            }
+
             // outputs
             if (ImGui::CollapsingHeader("outputs")) {
                 size_t i = 0;
@@ -1704,6 +1793,24 @@ namespace osmv {
                     ImGui::Columns();
                     ImGui::Separator();
                 }
+            }
+
+            // sockets
+            if (ImGui::CollapsingHeader("sockets")) {
+                std::vector<std::string> socknames = const_cast<OpenSim::Component&>(c).getSocketNames();
+                ImGui::Columns(2);
+                for (std::string const& sn : socknames) {
+                    ImGui::Text(sn.c_str());
+                    ImGui::NextColumn();
+
+                    std::string const& cp = c.getSocket(sn).getConnecteePath();
+                    ImGui::Text(cp.c_str());
+                    if (ImGui::IsItemClicked(ImGuiMouseButton_Right)) {
+                        selected_component = &c.getComponent(cp);
+                    }
+                    ImGui::NextColumn();
+                }
+                ImGui::Columns();
             }
         }
     };
