69d813f01553b093fbf505d1eb450557dcd076f3
Adam Kewley <contact@adamkewley.com>
2021-02-16 09:25:15 +0000

    Implemented hacky model editing (requires typing in etc)

--- src/model_editor_screen.cpp
+++ src/model_editor_screen.cpp
@@ -10,39 +10,132 @@
 
 #include <OpenSim/Simulation/Model/Model.h>
 #include <OpenSim/Simulation/SimbodyEngine/FreeJoint.h>
+#include <OpenSim/Simulation/SimbodyEngine/PinJoint.h>
 #include <imgui.h>
+
 #include <optional>
 
 namespace osmv {
     struct Model_editor_screen_impl final {
         OpenSim::Model model;
 
+        char meshname[128] = "block.vtp";
+
+        std::vector<OpenSim::Model> snapshots;
+        int snapshotidx = 0;
+
         int parentbodyidx = -1;
-        char bodyname[64] = "added_body";
-        char jointname[64] = "added_joint";
-
-        char jointTxName[64] = "xTranslation";
-        float jointTx = 0.0f;
-
-        char jointTyName[64] = "yTranslation";
-        float jointTy = 0.0f;
-
-        char jointTzName[64] = "zTranslation";
-        float jointTz = 0.0f;
 
         Simple_model_renderer renderer{app().window_dimensions().w, app().window_dimensions().h, app().samples()};
 
         osmv::State fdss{model.initSystem()};
         std::optional<Fd_simulator> fdsim;
-
-        double mass = 20.0;
-        SimTK::Vec3 centerOfMass{0.0};
         OpenSim::Component const* selected_component = nullptr;
 
         Model_editor_screen_impl() {
             renderer.flags |= SimpleModelRendererFlags_HoverableStaticDecorations;
             model.realizeReport(fdss);
             model.updDisplayHints().set_show_frames(true);
+        }
+
+        void draw_prop_editor() {
+            if (not selected_component) {
+                ImGui::Text("select something");
+                return;
+            }
+
+            // TODO: naughty naughty: this is because pointers to non-const els
+            // cannot be easily be coerced into points to const els (even though
+            // you'd expect one to be a logical subset of the other)
+            //
+            // see:
+            // https://stackoverflow.com/questions/36302078/non-const-reference-to-a-non-const-pointer-pointing-to-the-const-object
+            OpenSim::Component& c = const_cast<OpenSim::Component&>(*selected_component);
+
+            // properties
+            ImGui::Columns(2);
+
+            // edit name
+            {
+                ImGui::Text("name");
+                ImGui::NextColumn();
+
+                char nambuf[128];
+                nambuf[sizeof(nambuf) - 1] = '\0';
+                std::strncpy(nambuf, c.getName().c_str(), sizeof(nambuf) - 1);
+                if (ImGui::InputText("##nameditor", nambuf, sizeof(nambuf))) {
+                    c.setName(nambuf);
+                }
+                ImGui::NextColumn();
+            }
+
+            for (int i = 0; i < c.getNumProperties(); ++i) {
+                OpenSim::AbstractProperty& p = c.updPropertyByIndex(i);
+                ImGui::Text("%s", p.getName().c_str());
+                ImGui::NextColumn();
+
+                ImGui::PushID(p.getName().c_str());
+                bool editor_rendered = false;
+
+                // try string
+                {
+                    auto* sp = dynamic_cast<OpenSim::Property<std::string>*>(&p);
+                    if (sp) {
+                        char buf[64]{};
+                        buf[sizeof(buf) - 1] = '\0';
+                        std::strncpy(buf, sp->getValue().c_str(), sizeof(buf) - 1);
+
+                        ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x);
+                        ImGui::InputText("##stringeditor", buf, sizeof(buf));
+
+                        editor_rendered = true;
+                    }
+                }
+
+                // try double
+                {
+                    auto* dp = dynamic_cast<OpenSim::Property<double>*>(&p);
+                    if (dp and not dp->isListProperty()) {
+                        float v = static_cast<float>(dp->getValue());
+
+                        ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x);
+                        if (ImGui::InputFloat("##doubleditor", &v)) {
+                            dp->setValue(static_cast<double>(v));
+                        }
+
+                        editor_rendered = true;
+                    }
+                }
+
+                // try Vec3
+                {
+                    auto* vp = dynamic_cast<OpenSim::Property<SimTK::Vec3>*>(&p);
+                    if (vp and not vp->isListProperty()) {
+                        SimTK::Vec3 const& v = vp->getValue();
+                        float vs[3];
+                        vs[0] = static_cast<float>(v[0]);
+                        vs[1] = static_cast<float>(v[1]);
+                        vs[2] = static_cast<float>(v[2]);
+
+                        ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x);
+                        if (ImGui::InputFloat3("##vec3editor", vs)) {
+                            vp->setValue(SimTK::Vec3{
+                                static_cast<double>(vs[0]), static_cast<double>(vs[1]), static_cast<double>(vs[2])});
+                        }
+
+                        editor_rendered = true;
+                    }
+                }
+
+                if (not editor_rendered) {
+                    ImGui::Text("%s", p.toString().c_str());
+                }
+
+                ImGui::PopID();
+
+                ImGui::NextColumn();
+            }
+            ImGui::Columns();
         }
     };
 }
@@ -146,111 +239,99 @@ void osmv::Model_editor_screen::draw() {
     }
     ImGui::End();
 
+    // prop editor
+    if (ImGui::Begin("Edit Props")) {
+        impl->draw_prop_editor();
+    }
+    ImGui::End();
+
+    if (ImGui::Begin("Snapshots")) {
+        std::vector<std::string> names;
+        names.reserve(1 + impl->snapshots.size());
+
+        names.push_back("current");
+
+        for (size_t i = impl->snapshots.size(); i > 0; --i) {
+            names.push_back(std::to_string(i));
+        }
+
+        std::vector<char const*> cnames;
+        cnames.reserve(names.size());
+        for (std::string const& name : names) {
+            cnames.push_back(name.c_str());
+        }
+
+        if (ImGui::Combo("snapshots", &impl->snapshotidx, cnames.data(), static_cast<int>(cnames.size()))) {
+            if (impl->snapshotidx > 0) {
+                size_t idx = impl->snapshots.size() - static_cast<size_t>(impl->snapshotidx);
+
+                impl->model = impl->snapshots[idx];
+                impl->snapshotidx = 0;
+                impl->fdsim.reset();
+                impl->selected_component = nullptr;
+            }
+        }
+
+        if (ImGui::Button("take snapshot")) {
+            impl->snapshots.push_back(impl->model);
+        }
+    }
+    ImGui::End();
+
     // 'actions' ImGui panel
     //
     // this is a dumping ground for generic editing actions (add body, add something to selection)
     if (ImGui::Begin("Actions")) {
 
-        ImGui::Text("add joint");
-        ImGui::Separator();
-
-        ImGui::Text("joint type: FreeJoint");
-
-        // this body
-        ImGui::Text("body details:");
-        {
-            ImGui::InputText("name", impl->bodyname, sizeof(impl->bodyname));
-            {
-                float mass = static_cast<float>(impl->mass);
-                if (ImGui::SliderFloat("mass", &mass, 0.001f, 10.0f)) {
-                    impl->mass = static_cast<double>(mass);
-                }
-            }
-            {
-                float offset0 = static_cast<float>(impl->centerOfMass[0]);
-                if (ImGui::SliderFloat("com x", &offset0, -10.0f, 10.0f)) {
-                    impl->centerOfMass[0] = static_cast<double>(offset0);
-                }
-            }
-            {
-                float offset1 = static_cast<float>(impl->centerOfMass[1]);
-                if (ImGui::SliderFloat("com y", &offset1, -10.0f, 10.0f)) {
-                    impl->centerOfMass[1] = static_cast<double>(offset1);
-                }
-            }
-            {
-                float offset2 = static_cast<float>(impl->centerOfMass[2]);
-                if (ImGui::SliderFloat("com z", &offset2, -10.0f, 10.0f)) {
-                    impl->centerOfMass[2] = static_cast<double>(offset2);
-                }
-            }
+        if (impl->selected_component == nullptr) {
+            ImGui::Text("select something");
         }
 
-        // parent body
-        ImGui::Text("parent body details:");
-        OpenSim::PhysicalFrame const* parent = nullptr;
+        // if a physical frame is selected, show "add joint" option
         {
-            std::vector<char const*> names;
-            names.push_back("ground");
+            auto* pf = dynamic_cast<OpenSim::PhysicalFrame const*>(impl->selected_component);
+            if (pf) {
+                if (ImGui::Button("Add FreeJoint")) {
+                    auto* mesh = new OpenSim::Mesh{impl->meshname};
+                    auto* body = new OpenSim::Body{"added_body", 1.0, SimTK::Vec3{0.0}, SimTK::Inertia{0.1}};
+                    body->attachGeometry(mesh);
 
-            auto const& bs = model.getBodySet();
-            for (int i = 0; i < bs.getSize(); ++i) {
-                OpenSim::Body const* b = &bs[i];
-                names.push_back(b->getName().c_str());
-            }
+                    auto* joint = new OpenSim::FreeJoint{"freejoint", *pf, *body};
 
-            ImGui::Combo("parent", &impl->parentbodyidx, names.data(), static_cast<int>(names.size()));
+                    // SimTK::Vec3 offset{0.0, 0.0, 0.0};
+                    // auto* pof = new OpenSim::PhysicalOffsetFrame{"parent_offset", *parent, offset};
+                    // joint->addFrame(pof);
 
-            if (impl->parentbodyidx < 0) {
-                parent = nullptr;
-            } else if (impl->parentbodyidx == 0) {
-                parent = &model.getGround();
-            } else {
-                parent = &model.getBodySet()[impl->parentbodyidx - 1];
-            }
-        }
+                    // auto* cof = new OpenSim::PhysicalOffsetFrame{"child_offset", *body, offset};
+                    // joint->addFrame(cof);
 
-        ImGui::Text("joint details:");
-        {
-            ImGui::InputText("joint name", impl->jointname, sizeof(impl->jointname));
+                    auto& bs = model.updBodySet();
+                    bs.insert(bs.getSize(), body);
 
-            ImGui::InputText("tx coordname", impl->jointTxName, sizeof(impl->jointTxName));
-            ImGui::SliderFloat("tx", &impl->jointTx, -10.0f, 10.0f);
+                    auto& js = model.updJointSet();
+                    js.insert(js.getSize(), joint);
+                }
 
-            ImGui::InputText("ty coordname", impl->jointTyName, sizeof(impl->jointTyName));
-            ImGui::SliderFloat("ty", &impl->jointTy, -10.0f, 10.0f);
+                if (ImGui::Button("Add PinJoint")) {
+                    auto* mesh = new OpenSim::Mesh{impl->meshname};
+                    auto* body = new OpenSim::Body{"added_body", 1.0, SimTK::Vec3{0.0}, SimTK::Inertia{0.1}};
+                    body->attachGeometry(mesh);
 
-            ImGui::InputText("tz coordname", impl->jointTzName, sizeof(impl->jointTzName));
-            ImGui::SliderFloat("tz", &impl->jointTz, -10.0f, 10.0f);
-        }
+                    auto* joint = new OpenSim::PinJoint{"pinjoint", *pf, *body};
 
-        if (parent != nullptr) {
-            if (ImGui::Button("add")) {
-                auto* mesh = new OpenSim::Mesh{"block.vtp"};
-                auto* body = new OpenSim::Body{impl->bodyname, impl->mass, impl->centerOfMass, SimTK::Inertia{0.1}};
-                body->attachGeometry(mesh);
+                    // SimTK::Vec3 offset{0.0, 0.0, 0.0};
+                    // auto* pof = new OpenSim::PhysicalOffsetFrame{"parent_offset", *parent, offset};
+                    // joint->addFrame(pof);
 
-                auto* joint = new OpenSim::FreeJoint{impl->jointname, *parent, *body};
+                    // auto* cof = new OpenSim::PhysicalOffsetFrame{"child_offset", *body, offset};
+                    // joint->addFrame(cof);
 
-                // coordinate indices:
-                // [0-3): rot x y z
-                // [3-6): trans x y z
-                joint->upd_coordinates(3).set_default_value(static_cast<double>(impl->jointTx));
-                joint->upd_coordinates(4).set_default_value(static_cast<double>(impl->jointTy));
-                joint->upd_coordinates(5).set_default_value(static_cast<double>(impl->jointTz));
+                    auto& bs = model.updBodySet();
+                    bs.insert(bs.getSize(), body);
 
-                // SimTK::Vec3 offset{0.0, 0.0, 0.0};
-                // auto* pof = new OpenSim::PhysicalOffsetFrame{"parent_offset", *parent, offset};
-                // joint->addFrame(pof);
-
-                // auto* cof = new OpenSim::PhysicalOffsetFrame{"child_offset", *body, offset};
-                // joint->addFrame(cof);
-
-                auto& bs = model.updBodySet();
-                bs.insert(bs.getSize(), body);
-
-                auto& js = model.updJointSet();
-                js.insert(js.getSize(), joint);
+                    auto& js = model.updJointSet();
+                    js.insert(js.getSize(), joint);
+                }
             }
         }
     }
