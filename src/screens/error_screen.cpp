270587e773c1d02a2b94f3ef972876019a19c578
Adam Kewley <contact@adamkewley.com>
2021-03-26 13:08:55 +0100

    Improved error screen layout and added log to it

--- src/screens/error_screen.cpp
+++ src/screens/error_screen.cpp
@@ -2,6 +2,7 @@
 
 #include "src/application.hpp"
 #include "src/screens/splash_screen.hpp"
+#include "src/widgets/log_viewer_widget.hpp"
 
 #include <SDL_keyboard.h>
 #include <SDL_keycode.h>
@@ -14,6 +15,10 @@ using namespace osmv;
 
 struct Error_screen::Impl final {
     std::string msg;
+    Log_viewer_widget_state log;
+
+    Impl(std::string _msg) : msg{std::move(_msg)} {
+    }
 };
 
 Error_screen::Error_screen(std::exception const& ex) : impl{new Impl{ex.what()}} {
@@ -32,12 +37,38 @@ bool Error_screen::on_event(SDL_Event const& e) {
 }
 
 void Error_screen::draw() {
-    if (ImGui::Begin("fatal exception")) {
-        ImGui::Text("The application threw an exception with the following message:");
+    constexpr float width = 800.0f;
+    constexpr float padding = 10.0f;
+
+    // center error near top of screen
+    {
+        ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+        center.y = padding;
+        ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.0f));
+        ImGui::SetNextWindowSize(ImVec2(width, 0.0f));
+    }
+
+    if (ImGui::Begin("fatal error")) {
+        ImGui::TextWrapped("The application threw an exception with the following message:");
         ImGui::Dummy(ImVec2{0.0f, 10.0f});
-        ImGui::Text("%s", impl->msg.c_str());
+        ImGui::Dummy(ImVec2{2.0f, 0.0f});
+        ImGui::SameLine();
+        ImGui::TextWrapped("%s", impl->msg.c_str());
         ImGui::Dummy(ImVec2{0.0f, 10.0f});
-        ImGui::Text("(press ESC to return to the splash screen)");
+
+        if (ImGui::Button("Return to splash screen (Escape)")) {
+            Application::current().request_screen_transition<Splash_screen>();
+        }
     }
     ImGui::End();
+
+    // center log at bottom of screen
+    {
+        ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+        center.y = ImGui::GetMainViewport()->Size.y - padding;
+        ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 1.0f));
+        ImGui::SetNextWindowSize(ImVec2(width, 0.0f));
+    }
+
+    draw_log_viewer_widget(impl->log, "log");
 }
