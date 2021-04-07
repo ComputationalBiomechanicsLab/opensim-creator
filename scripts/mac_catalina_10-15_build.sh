af5baf04302b3b8e8c7d0313128f9b71668d8542
Adam Kewley <contact@adamkewley.com>
2021-04-07 10:36:21 +0100

    Updated Mac Catalina build to use OpenSim 4.2

--- scripts/mac_catalina_10-15_build.sh
+++ scripts/mac_catalina_10-15_build.sh
@@ -7,7 +7,7 @@ skip_opensim_download=${skip_opensim_download:0}
 
 # (if building OpenSim): get OpenSim 4.1 source
 if [[ "${skip_opensim_download}" -eq "0" ]]; then
-    git clone --single-branch --branch 4.1 --depth=1 https://github.com/opensim-org/opensim-core
+    git clone --single-branch --branch 4.2 --depth=1 https://github.com/opensim-org/opensim-core
 fi
 
 # ----- build: build OpenSim (optional) then build osmv ----- #
@@ -15,19 +15,32 @@ fi
 # (if building OpenSim): build OpenSim's dependencies
 mkdir -p opensim-dependencies-build/
 cd opensim-dependencies-build/
-cmake ../opensim-core/dependencies -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../opensim-dependencies-install
+cmake ../opensim-core/dependencies \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=../opensim-dependencies-install \
+    -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer"
 cmake --build . -- -j ${num_workers}
 cd -
 
 # (if building OpenSim): build OpenSim
 mkdir -p opensim-build/
 cd opensim-build/
-cmake ../opensim-core/ -DOPENSIM_DEPENDENCIES_DIR=../opensim-dependencies-install/ -DCMAKE_INSTALL_PREFIX=../opensim-install/ -DBUILD_JAVA_WRAPPING=OFF -DCMAKE_BUILD_TYPE=Release
+cmake ../opensim-core/ \
+    -DOPENSIM_DEPENDENCIES_DIR=../opensim-dependencies-install/ \
+    -DCMAKE_INSTALL_PREFIX=../opensim-install/ \
+    -DBUILD_JAVA_WRAPPING=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer" \
+    -DOPENSIM_WITH_CASADI=NO \
+    -DOPENSIM_WITH_TROPTER=NO
 cmake --build . --target install -- -j ${num_workers}
 cd -
 
+# build OSMV
 mkdir -p osmv-build/
 cd osmv-build/
-cmake .. -DCMAKE_PREFIX_PATH=${PWD}/../opensim-install -DCMAKE_BUILD_TYPE=Release
+cmake .. \
+    -DCMAKE_PREFIX_PATH=${PWD}/../opensim-install \
+    -DCMAKE_BUILD_TYPE=Release
 cmake --build . --target package -- -j ${num_workers}
 
