a8891675a0e06aff7f2a57aad4b60affb598eef8
Adam Kewley <contact@adamkewley.com>
2021-04-29 16:06:11 +0200

    Made some extension methods noexcept

--- src/3d/gl_extensions.hpp
+++ src/3d/gl_extensions.hpp
@@ -10,7 +10,7 @@
 
 // gl_extensions: stuff that isn't strictly required to use OpenGL, but it nice to have
 namespace gl {
-    inline void Uniform(Uniform_int const& u, GLsizei n, GLint const* data) {
+    inline void Uniform(Uniform_int const& u, GLsizei n, GLint const* data) noexcept {
         glUniform1iv(u.geti(), n, data);
     }
 
@@ -30,7 +30,7 @@ namespace gl {
         glUniform3f(u.geti(), x, y, z);
     }
 
-    inline void Uniform(Uniform_vec3& u, float const vs[3]) {
+    inline void Uniform(Uniform_vec3& u, float const vs[3]) noexcept {
         glUniform3fv(u.geti(), 1, vs);
     }
 
@@ -42,7 +42,7 @@ namespace gl {
     // set a uniform array of vec3s from a userspace container type (e.g. vector<glm::vec3>)
     template<typename Container, size_t N>
     inline std::enable_if_t<std::is_same_v<glm::vec3, typename Container::value_type>, void>
-        Uniform(Uniform_array<glsl::vec3, N>& u, Container& container) {
+        Uniform(Uniform_array<glsl::vec3, N>& u, Container& container) noexcept {
         assert(container.size() == N);
         glUniform3fv(u.geti(), static_cast<GLsizei>(container.size()), glm::value_ptr(*container.data()));
     }
@@ -74,23 +74,23 @@ namespace gl {
 
     template<typename Container, size_t N>
     inline std::enable_if_t<std::is_same_v<glm::vec2, typename Container::value_type>, void>
-        Uniform(Uniform_array<glsl::vec2, N>& u, Container const& container) {
+        Uniform(Uniform_array<glsl::vec2, N>& u, Container const& container) noexcept {
         glUniform2fv(u.geti(), static_cast<GLsizei>(container.size()), glm::value_ptr(container.data()));
     }
 
-    inline void Uniform(Uniform_sampler2d& u, GLint v) {
+    inline void Uniform(Uniform_sampler2d& u, GLint v) noexcept {
         glUniform1i(u.geti(), v);
     }
 
-    inline void Uniform(Uniform_sampler2DMS& u, GLint v) {
+    inline void Uniform(Uniform_sampler2DMS& u, GLint v) noexcept {
         glUniform1i(u.geti(), v);
     }
 
-    inline void Uniform(Uniform_bool& u, bool v) {
+    inline void Uniform(Uniform_bool& u, bool v) noexcept {
         glUniform1i(u.geti(), v);
     }
 
-    inline void ClearColor(glm::vec4 const& v) {
+    inline void ClearColor(glm::vec4 const& v) noexcept {
         ClearColor(v[0], v[1], v[2], v[3]);
     }
 }
