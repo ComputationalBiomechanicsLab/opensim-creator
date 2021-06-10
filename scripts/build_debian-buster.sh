f92871627d83b0b4a3d256520d2b1c8c0424aafa
Adam Kewley <contact@adamkewley.com>
2021-06-10 15:07:00 +0200

    Made OpenSim repo URL externally overridable in Linux build

--- scripts/build_debian-buster.sh
+++ scripts/build_debian-buster.sh
@@ -17,7 +17,7 @@ set -xeuo pipefail
 # where to clone the OpenSim source from
 #
 # handy to override if you are developing against a fork, locally, etc.
-OSC_OPENSIM_REPO=https://github.com/opensim-org/opensim-core
+OSC_OPENSIM_REPO=${OSC_OPENSIM_REPO:-https://github.com/opensim-org/opensim-core}
 
 # can be any branch identifier from opensim-core
 OSC_OPENSIM_REPO_BRANCH=${OSC_OPENSIM_VERSION:-4.2}
