6904c64ad8637d8c6c3ea6831246c7a79d5d5fab
Adam Kewley <contact@adamkewley.com>
2021-08-03 14:51:51 +0200

    Move fd param documentation into the bindings header itself

--- src/opensim_bindings/simulation.hpp
+++ src/opensim_bindings/simulation.hpp
@@ -33,15 +33,23 @@ namespace osc::fd {
 
         // final time for the simulation
         std::chrono::duration<double> final_time{10.0};
+        static constexpr char const* final_time_title = "final time (sec)";
+        static constexpr char const* final_time_desc = "The final time, in seconds, that the forward dynamic simulation should integrate up to";
 
         // true if the simulation should slow down whenever it runs faster than wall-time
         bool throttle_to_wall_time = true;
+        static constexpr char const* throttle_to_wall_time_title = "throttle to wall time";
+        static constexpr char const* throttle_to_wall_time_desc = "Whether the simulator should slow down whenever it is running faster than real time. This is useful for visualizing the simulation 'as it runs' - especially when the simulation would complete much faster than the simulation time";
 
         // which integration method to use for the simulation
         IntegratorMethod integrator_method = IntegratorMethod_OpenSimManagerDefault;
+        static constexpr char const* integrator_method_title = "integrator method";
+        static constexpr char const* integrator_method_desc = "The integrator that the forward dynamic simulator should use. OpenSim's default integrator is a good choice if you aren't familiar with the other integrators. Changing the integrator can have a large impact on the performance and accuracy of the simulation.";
 
         // the time interval, in simulation time, between report updates
         std::chrono::duration<double> reporting_interval{1.0/120.0};
+        static constexpr char const* reporting_interval_title = "reporting interval";
+        static constexpr char const* reporting_interval_desc = "How often the simulator should emit a simulation report. This affects how many datapoints are collected for the animation, output values, etc.";
 
         // max number of *internal* steps that may be taken within a single call
         // to the integrator's stepTo or stepBy function
@@ -50,11 +58,15 @@ namespace osc::fd {
         // simulator reports updates (e.g. a lower number here *may* mean more
         // frequent per-significant-step updates)
         int integrator_step_limit = 20000;
+        static constexpr char const* integrator_step_limit_title = "integrator step limit";
+        static constexpr char const* integrator_step_limit_desc = "The maximum number of *internal* steps that can be taken within a single call to the integrator's stepTo/stepBy function. This is mostly an internal engine concern, but can occasionally affect how often reports are emitted";
 
         // minimum step, in time, that the integrator should attempt
         //
         // some integrators just ignore this
         std::chrono::duration<double> integrator_minimum_step_size{1.0e-8};
+        static constexpr char const* integrator_minimum_step_size_title = "integrator minimum step size (sec)";
+        static constexpr char const* integrator_minimum_step_size_desc = "The minimum step size, in time, that the integrator must take during the simulation. Note: this is mostly only relevant for error-corrected integrators that change their step size dynamically as the simulation runs.";
 
         // maximum step, in time, that an integrator can attempt
         //
@@ -62,12 +74,16 @@ namespace osc::fd {
         // it still *must* integrate to this size and return to the caller (i.e. the
         // simulator) to report the state at this maximum time
         std::chrono::duration<double> integrator_maximum_step_size{1.0};
+        static constexpr char const* integrator_maximum_step_size_title = "integrator maximum step size (sec)";
+        static constexpr char const* integrator_maximum_step_size_desc = "The maximum step size, in seconds, that the integrator must take during the simulation. Note: this is mostly only relevant for error-correct integrators that change their step size dynamically as the simulation runs";
 
         // accuracy of the integrator
         //
         // this only does something if the integrator is error-controlled and able
         // to improve accuracy (e.g. by taking many more steps)
         double integrator_accuracy = 1.0e-5;
+        static constexpr char const* integrator_accuracy_title = "integrator accuracy";
+        static constexpr char const* integrator_accuracy_desc = "Target accuracy for the integrator. Mostly only relevant for error-controlled integrators that change their step size by comparing this accuracy value to measured integration error";
 
         // set whether the latest state update from the simulator should be posted
         // on every step (if not yet popped)
@@ -75,6 +91,8 @@ namespace osc::fd {
         // else: the update is only posted whenever the regular reporting interval
         // is set
         bool update_latest_state_on_every_step = true;
+        static constexpr char const* update_latest_state_on_every_step_title = "update latest state on every step";
+        static constexpr char const* update_latest_state_on_every_step_desc = "Whether the simulator should try to update the latest integration state on each integration step. Internally, the UI will frequently ask the simulator for the latest state *and* regular reports (defined above). The latest state is only really used to provide a smooth simulation playback. Disabling this may improve simulation performance (because the simulator will only have to post updates at the regular reporting interval).";
     };
 
     // simulation input
