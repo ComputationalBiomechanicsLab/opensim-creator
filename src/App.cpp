9882426063e9dd6569300f7f6a38fd93ca8a5222
Travis CI <travis@travis-ci.org>
2021-09-24 16:21:16 +0200

    Change font to fa-solid-900.ttf

--- src/App.cpp
+++ src/App.cpp
@@ -975,11 +975,13 @@ void osc::ImGuiInit() {
         io.Fonts->AddFontDefault();
         ImFontConfig config;
         config.MergeMode = true;
-        config.GlyphMinAdvanceX = 13.0f;  // monospaced
+        config.PixelSnapH = true;
         static const ImWchar icon_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
-        std::string fa_font = App::resource("fontawesome-webfont.ttf").string();
-        ImGui::GetIO().Fonts->AddFontFromFileTTF(fa_font.c_str(), 13.0f, &config, icon_ranges);
+        char const* file = "fa-solid-900.ttf";
+        std::string fontFile = App::resource(file).string();
+        ImGui::GetIO().Fonts->AddFontFromFileTTF(fontFile.c_str(), 12.0f, &config, icon_ranges);
     }
+
 
     // init ImGui for SDL2 /w OpenGL
     ImGui_ImplSDL2_InitForOpenGL(App::cur().m_Impl->window, App::cur().m_Impl->gl);
