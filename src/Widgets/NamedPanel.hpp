94589bd7f50b9b5562652afe6d03221f6af26c2d
Adam Kewley <contact@adamkewley.com>
2022-04-29 09:58:21 +0200

    Added osc::NamedPanel base class

---
+++ src/Widgets/NamedPanel.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace osc
+{
+	class NamedPanel {
+	public:
+		explicit NamedPanel(std::string_view name);
+		NamedPanel(std::string_view name, int imGuiWindowFlags);
+
+		virtual ~NamedPanel() noexcept = default;
+
+		bool isOpen() const;
+		void open();
+		void close();
+		void draw();
+
+	protected:
+		void requestClose();
+
+	private:
+		virtual void implDraw() = 0;
+
+		std::string m_PanelName;
+		int m_PanelFlags;
+	};
+}
