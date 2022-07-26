954f384374ec813edfd55cc6eadaa9d584cabe83
Adam Kewley <contact@adamkewley.com>
2022-07-26 11:53:11 +0200

    Implemented LightingMaps using renderer API

---
+++ resources/shaders/ExperimentLightingMaps.frag
@@ -0,0 +1,41 @@
+#version 330 core
+
+uniform vec3 uViewPos;
+uniform vec3 uLightPos;
+uniform vec3 uLightAmbient;
+uniform vec3 uLightDiffuse;
+uniform vec3 uLightSpecular;
+uniform sampler2D uMaterialDiffuse;
+uniform sampler2D uMaterialSpecular;
+uniform float uMaterialShininess;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+out vec4 FragColor;
+
+void main()
+{
+    vec3 diffuseColor = texture(uMaterialDiffuse, TexCoords).rgb;
+    vec3 specularColor = texture(uMaterialSpecular, TexCoords).rgb;
+
+    vec3 ambient = uLightAmbient * diffuseColor;
+
+    // compute ambient strength (ambient + diffuse)
+    vec3 frag2light = normalize(uLightPos - FragPos);
+    float diffuseStrength = max(dot(Normal, frag2light), 0.0);
+    vec3 diffuse = diffuseStrength * uLightDiffuse * diffuseColor;
+
+    // specular
+    vec3 frag2View = normalize(uViewPos - FragPos);
+    vec3 light2frag = -frag2light;
+    vec3 frag2LightReflection = reflect(light2frag, Normal);
+    float specularStrength = pow(max(dot(frag2View, frag2LightReflection), 0.0), uMaterialShininess);
+    vec3 specular = specularStrength * uLightSpecular * specularColor;
+
+    vec3 result = ambient + diffuse + specular;
+
+    FragColor = vec4(result, 1.0);
+}
+
--- src/Tabs/Experiments/RendererLightingMapsTab.cpp
+++ src/Tabs/Experiments/RendererLightingMapsTab.cpp
@@ -1,17 +1,44 @@
 #include "RendererLightingMapsTab.hpp"
 
+#include "src/Bindings/ImGuiHelpers.hpp"
+#include "src/Graphics/MeshGen.hpp"
 #include "src/Graphics/Renderer.hpp"
+#include "src/Maths/Transform.hpp"
+#include "src/Platform/App.hpp"
 #include "src/Utils/CStringView.hpp"
 #include "src/Utils/UID.hpp"
 
+#include <glm/vec3.hpp>
 #include <SDL_events.h>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <utility>
+
+// generate a texture-mapped cube
+static osc::experimental::Mesh GenerateMesh()
+{
+    osc::MeshData cube = osc::GenCube();
+
+    for (glm::vec3& vert : cube.verts)
+    {
+        vert *= 0.5f;  // makes the verts match LearnOpenGL
+    }
+
+    return osc::experimental::LoadMeshFromMeshData(cube);
+}
 
 class osc::RendererLightingMapsTab::Impl final {
 public:
     Impl(TabHost* parent) : m_Parent{parent}
     {
+        m_LightingMapsMaterial.setTexture("uMaterialDiffuse", m_DiffuseMap);
+        m_LightingMapsMaterial.setTexture("uMaterialSpecular", m_SpecularMap);
+        m_Camera.setPosition({0.0f, 0.0f, 3.0f});
+        m_Camera.setCameraFOV(glm::radians(45.0f));
+        m_Camera.setNearClippingPlane(0.1f);
+        m_Camera.setFarClippingPlane(100.0f);
+        m_LightTransform.position = {0.4f, 0.4f, 2.0f};
+        m_LightTransform.scale = {0.2f, 0.2f, 0.2f};
     }
 
     UID getID() const
@@ -31,14 +58,29 @@ public:
 
     void onMount()
     {
+        App::upd().makeMainEventLoopPolling();
+        m_IsMouseCaptured = true;
     }
 
     void onUnmount()
     {
+        m_IsMouseCaptured = false;
+        App::upd().setShowCursor(true);
+        App::upd().makeMainEventLoopWaiting();
     }
 
     bool onEvent(SDL_Event const& e)
     {
+        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)
+        {
+            m_IsMouseCaptured = false;
+            return true;
+        }
+        else if (e.type == SDL_MOUSEBUTTONDOWN && osc::IsMouseInMainViewportWorkspaceScreenRect())
+        {
+            m_IsMouseCaptured = true;
+            return true;
+        }
         return false;
     }
 
@@ -52,13 +94,83 @@ public:
 
     void onDraw()
     {
+        // handle mouse capturing
+        if (m_IsMouseCaptured)
+        {
+            UpdateEulerCameraFromImGuiUserInput(m_Camera, m_CameraEulers);
+            ImGui::SetMouseCursor(ImGuiMouseCursor_None);
+            App::upd().setShowCursor(false);
+        }
+        else
+        {
+            ImGui::SetMouseCursor(ImGuiMouseCursor_Arrow);
+            App::upd().setShowCursor(true);
+        }
+
+        // clear screen and ensure camera has correct pixel rect
+        App::upd().clearScreen({0.1f, 0.1f, 0.1f, 1.0f});
+        m_Camera.setPixelRect(osc::GetMainViewportWorkspaceScreenRect());
+
+        // draw cube
+        m_LightingMapsMaterial.setVec3("uViewPos", m_Camera.getPosition());
+        m_LightingMapsMaterial.setVec3("uLightPos", m_LightTransform.position);
+        m_LightingMapsMaterial.setVec3("uLightAmbient", m_LightAmbient);
+        m_LightingMapsMaterial.setVec3("uLightDiffuse", m_LightDiffuse);
+        m_LightingMapsMaterial.setVec3("uLightSpecular", m_LightSpecular);
+        m_LightingMapsMaterial.setFloat("uMaterialShininess", m_MaterialShininess);
+        experimental::Graphics::DrawMesh(m_Mesh, Transform{}, m_LightingMapsMaterial, m_Camera);
+
+        // draw lamp
+        m_LightCubeMaterial.setVec3("uLightColor", {1.0f, 1.0f, 1.0f});
+        osc::experimental::Graphics::DrawMesh(m_Mesh, m_LightTransform, m_LightCubeMaterial, m_Camera);
+
+        // render to output (window)
+        m_Camera.render();
+
+        // render auxiliary UI
+        ImGui::Begin("controls");
+        ImGui::InputFloat3("uLightPos", glm::value_ptr(m_LightTransform.position));
+        ImGui::InputFloat3("uLightAmbient", glm::value_ptr(m_LightAmbient));
+        ImGui::InputFloat3("uLightDiffuse", glm::value_ptr(m_LightDiffuse));
+        ImGui::InputFloat3("uLightSpecular", glm::value_ptr(m_LightSpecular));
+        ImGui::InputFloat("uMaterialShininess", &m_MaterialShininess);
+        ImGui::End();
     }
 
 private:
     UID m_ID;
     TabHost* m_Parent;
-    experimental::Texture2D m_ContainerColor = experimental::LoadTexture2DFromImageResource("textures/container2.png", ImageFlags_FlipVertically);
-    experimental::Texture2D m_ContainerSpecular = experimental::LoadTexture2DFromImageResource("textures/container2_specular.png", ImageFlags_FlipVertically);
+
+    experimental::Material m_LightingMapsMaterial
+    {
+        experimental::Shader
+        {
+            App::slurp("shaders/ExperimentLightingMaps.vert"),
+            App::slurp("shaders/ExperimentLightingMaps.frag"),
+        }
+    };
+    experimental::Material m_LightCubeMaterial
+    {
+        experimental::Shader
+        {
+            App::slurp("shaders/ExperimentLightCube.vert"),
+            App::slurp("shaders/ExperimentLightCube.frag"),
+        }
+    };
+    experimental::Mesh m_Mesh = GenerateMesh();
+    experimental::Texture2D m_DiffuseMap = experimental::LoadTexture2DFromImageResource("textures/container2.png", ImageFlags_FlipVertically);
+    experimental::Texture2D m_SpecularMap = experimental::LoadTexture2DFromImageResource("textures/container2_specular.png", ImageFlags_FlipVertically);
+
+    experimental::Camera m_Camera;
+    glm::vec3 m_CameraEulers = {0.0f, 0.0f, 0.0f};
+    bool m_IsMouseCaptured = false;
+
+    Transform m_LightTransform;
+    glm::vec3 m_LightAmbient = {0.2f, 0.2f, 0.2f};
+    glm::vec3 m_LightDiffuse = {0.5f, 0.5f, 0.5f};
+    glm::vec3 m_LightSpecular = {1.0f, 1.0f, 1.0f};
+
+    float m_MaterialShininess = 64.0f;
 };
 
 
