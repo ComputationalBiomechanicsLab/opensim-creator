f7c8fb2fc26fb5615927e7b37d833431d3bdac85
Adam Kewley <contact@adamkewley.com>
2022-08-01 07:31:26 +0200

    Reordered Transform members to match usage order

--- src/Maths/Transform.hpp
+++ src/Maths/Transform.hpp
@@ -9,10 +9,11 @@ namespace osc
 {
     // packaged-up "transform" (orthogonal scale -> rotate -> translate)
     struct Transform final {
-        glm::vec3 position{0.0f, 0.0f, 0.0f};
-        glm::quat rotation{1.0f, 0.0f, 0.0f, 0.0f};
-        glm::vec3 scale{1.0f, 1.0f, 1.0f};
-    
+
+        glm::vec3 scale = {1.0f, 1.0f, 1.0f};
+        glm::quat rotation = {1.0f, 0.0f, 0.0f, 0.0f};
+        glm::vec3 position = {0.0f, 0.0f, 0.0f};
+
         // default-construct as an identity transform
         constexpr Transform() = default;
     
@@ -29,7 +30,7 @@ namespace osc
             rotation{rotation_}
         {
         }
-    
+
         // construct at a given position, rotation, and scale
         constexpr Transform(glm::vec3 const& position_,
                             glm::quat const& rotation_,
@@ -39,28 +40,28 @@ namespace osc
             scale{scale_}
         {
         }
-    
+
         // returns a new transform which is the same as the existing one, but with the
         // provided position
         constexpr Transform withPosition(glm::vec3 const& position_) const noexcept
         {
             return Transform{position_, rotation, scale};
         }
-    
+
         // returns a new transform which is the same as the existing one, but with
         // the provided rotation
         constexpr Transform withRotation(glm::quat const& rotation_) const noexcept
         {
             return Transform{position, rotation_, scale};
         }
-    
+
         // returns a new transform which is the same as the existing one, but with
         // the provided scale
         constexpr Transform withScale(glm::vec3 const& scale_) const noexcept
         {
             return Transform{position, rotation, scale_};
         }
-    
+
         // returns a new transform which is the same as the existing one, but with
         // the provided scale (same for all axes)
         constexpr Transform withScale(float scale_) const noexcept
@@ -68,16 +69,16 @@ namespace osc
             return Transform{position, rotation, {scale_, scale_, scale_}};
         }
     };
-    
+
     // pretty-prints a `Transform` for readability
     std::ostream& operator<<(std::ostream& o, Transform const&);
 
     // applies the transform to a point vector (equivalent to `TransformPoint`)
     glm::vec3 operator*(Transform const&, glm::vec3 const&) noexcept;
-    
+
     // performs component-wise addition of two transforms
     Transform& operator+=(Transform&, Transform const&) noexcept;
-    
+
     // performs component-wise scalar division
     Transform& operator/=(Transform&, float) noexcept;
 }
