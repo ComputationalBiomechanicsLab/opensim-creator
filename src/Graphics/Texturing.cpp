f39c6b05dd99521a1da70ebc8402194a89ec785f
Adam Kewley <contact@adamkewley.com>
2022-08-08 17:43:30 +0200

    Removed unused LoadCubemapAsCubemapTexture

--- src/Graphics/Texturing.cpp
+++ src/Graphics/Texturing.cpp
@@ -16,37 +16,6 @@
 #include <string>
 
 
-// helper method: load a file into an image and send it to OpenGL
-static void LoadCubemapSurface(std::filesystem::path const& path, GLenum target, osc::ImageFlags flags)
-{
-    auto img = osc::Image::Load(path, flags);
-
-    GLenum format;
-    switch (img.getNumChannels())
-    {
-    case 1:
-        format = GL_RED;
-        break;
-    case 3:
-        format = GL_RGB;
-        break;
-    case 4:
-        format = GL_RGBA;
-        break;
-    default:
-    {
-        std::stringstream msg;
-        msg << path << ": error: contains " << img.getNumChannels()
-            << " color channels (the implementation doesn't know how to handle this)";
-        throw std::runtime_error{std::move(msg).str()};
-    }
-    }
-
-    glm::ivec2 dims = img.getDimensions();
-    glTexImage2D(target, 0, format, dims.x, dims.y, 0, format, GL_UNSIGNED_BYTE, img.getPixelData().data());
-}
-
-
 // public API
 
 gl::Texture2D osc::GenChequeredFloorTexture()
@@ -123,44 +92,4 @@ osc::ImageTexture osc::LoadImageAsTexture(std::filesystem::path const& path, Ima
     glGenerateMipmap(t.type);
 
     return ImageTexture{std::move(t), dims, numChannels};
-}
-
-gl::TextureCubemap osc::LoadCubemapAsCubemapTexture(
-    std::filesystem::path const& posX,
-    std::filesystem::path const& negX,
-    std::filesystem::path const& posY,
-    std::filesystem::path const& negY,
-    std::filesystem::path const& posZ,
-    std::filesystem::path const& negZ,
-    ImageFlags flags)
-{
-
-    gl::TextureCubemap rv;
-    gl::BindTexture(rv);
-
-    LoadCubemapSurface(posX, GL_TEXTURE_CUBE_MAP_POSITIVE_X, flags);
-    LoadCubemapSurface(negX, GL_TEXTURE_CUBE_MAP_NEGATIVE_X, flags);
-    LoadCubemapSurface(posY, GL_TEXTURE_CUBE_MAP_POSITIVE_Y, flags);
-    LoadCubemapSurface(negY, GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, flags);
-    LoadCubemapSurface(posZ, GL_TEXTURE_CUBE_MAP_POSITIVE_Z, flags);
-    LoadCubemapSurface(negZ, GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, flags);
-
-    /**
-     * From: https://learnopengl.com/Advanced-OpenGL/Cubemaps
-     *
-     * Don't be scared by the GL_TEXTURE_WRAP_R, this simply sets the wrapping
-     * method for the texture's R coordinate which corresponds to the texture's
-     * 3rd dimension (like z for positions). We set the wrapping method to
-     * GL_CLAMP_TO_EDGE since texture coordinates that are exactly between two
-     * faces may not hit an exact face (due to some hardware limitations) so
-     * by using GL_CLAMP_TO_EDGE OpenGL always returns their edge values
-     * whenever we sample between faces.
-     */
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-    return rv;
 }
--- src/Graphics/Texturing.hpp
+++ src/Graphics/Texturing.hpp
@@ -23,16 +23,4 @@ namespace osc
 
     // read an image file (.PNG, .JPEG, etc.) directly into an OpenGL (GPU) texture
     ImageTexture LoadImageAsTexture(std::filesystem::path const&, ImageFlags = ImageFlags_None);
-
-    // read 6 image files into a single OpenGL cubemap (GL_TEXTURE_CUBE_MAP)
-    //
-    // useful for skyboxes, precomputed point-shadow maps, etc.
-    gl::TextureCubemap LoadCubemapAsCubemapTexture(
-        std::filesystem::path const& posX,
-        std::filesystem::path const& negX,
-        std::filesystem::path const& posY,
-        std::filesystem::path const& negY,
-        std::filesystem::path const& posZ,
-        std::filesystem::path const& negZ,
-        ImageFlags = ImageFlags_None);
 }
