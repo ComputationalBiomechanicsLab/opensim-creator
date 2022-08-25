15bed6a106481f37d2af3c42c38841cc8c28d9be
Adam Kewley <contact@adamkewley.com>
2022-08-25 14:46:34 +0200

    Perf-optimized ReassignSocketPopup (#384)

--- src/Widgets/ReassignSocketPopup.cpp
+++ src/Widgets/ReassignSocketPopup.cpp
@@ -16,9 +16,98 @@
 #include <OpenSim/Simulation/Model/Model.h>
 
 #include <memory>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <utility>
+
+namespace
+{
+    // parameters that affect which sockets are displayed
+    struct PopupParams final {
+        osc::UID modelVersion;
+        OpenSim::ComponentPath path;
+        std::string socketName;
+        std::string search;
+
+        PopupParams(
+            osc::UID modelVersion_,
+            OpenSim::ComponentPath path_,
+            std::string socketName_) :
+
+            modelVersion{std::move(modelVersion_)},
+            path{std::move(path_)},
+            socketName{std::move(socketName_)}
+        {
+        }
+    };
+
+    bool operator==(PopupParams const& a, PopupParams const& b)
+    {
+        return
+            a.modelVersion == b.modelVersion &&
+            a.path == b.path &&
+            a.socketName == b.socketName &&
+            a.search == b.search;
+    }
+
+    bool operator!=(PopupParams const& a, PopupParams const& b)
+    {
+        return !(a == b);
+    }
+
+    // a single user-selectable connectee option
+    struct ConnecteeOption final {
+        OpenSim::ComponentPath absPath;
+        std::string name;
+
+        explicit ConnecteeOption(OpenSim::Component const& c) :
+            absPath{c.getAbsolutePath()},
+            name{c.getName()}
+        {
+        }
+    };
+
+    // generate a list of possible connectee options, given a set of popup parameters
+    std::vector<ConnecteeOption> GenerateSelectionOptions(OpenSim::Model const& model, PopupParams const& params)
+    {
+        std::vector<ConnecteeOption> rv;
+
+        OpenSim::Component const* component = osc::FindComponent(model, params.path);
+        if (!component)
+        {
+            return rv;   // component isn't in model?
+        }
+
+        OpenSim::AbstractSocket const* socket = osc::FindSocket(*component, params.socketName);
+        if (!socket)
+        {
+            return rv;  // socket isn't in model?
+        }
+
+        for (OpenSim::Component const& other : model.getComponentList())
+        {
+            if (&other == component)
+            {
+                continue;  // hide redundant reconnnections
+            }
+
+            if (!osc::ContainsSubstring(other.getName(), params.search))
+            {
+                continue;  // filtered out by search string
+            }
+
+            if (!osc::IsAbleToConnectTo(*socket, other))
+            {
+                continue;  // connection would be rejected anyway
+            }
+
+            rv.emplace_back(other);
+        }
+
+        return rv;
+    }
+}
 
 class osc::ReassignSocketPopup::Impl final : public osc::StandardPopup {
 public:
@@ -29,29 +118,41 @@ public:
 
         StandardPopup{std::move(popupName)},
         m_Model{std::move(model)},
-        m_ComponentPath{std::string{componentAbsPath}},
-        m_SocketName{std::move(socketName)}
+        m_Params{m_Model->getModelVersion(), std::string{componentAbsPath}, std::string{socketName}}
     {
     }
 
 private:
     void implDraw() override
     {
-        // ensure the "from" side of the socket still exists etc.
-        OpenSim::Component const* component = osc::FindComponent(m_Model->getModel(), m_ComponentPath);
+        // caching: regenerate cached socket list, if necessary
+        //
+        // we cache the list because searching+filtering all possible connectees is
+        // very slow in OpenSim (#384)
+        m_EditedParams.modelVersion = m_Model->getModelVersion();
+        if (m_EditedParams != m_Params)
+        {
+            m_Options = GenerateSelectionOptions(m_Model->getModel(), m_EditedParams);
+            m_Params = m_EditedParams;
+        }
+
+        // check: ensure the "from" side of the socket still exists
+        OpenSim::Component const* component = osc::FindComponent(m_Model->getModel(), m_Params.path);
         if (!component)
         {
             requestClose();
             return;
         }
 
-        // ensure the socket still exists
-        OpenSim::AbstractSocket const* socket = osc::FindSocket(*component, m_SocketName);
+        // check: ensure the socket still exists
+        OpenSim::AbstractSocket const* socket = osc::FindSocket(*component, m_Params.socketName);
         if (!socket)
         {
             requestClose();
             return;
         }
+
+        // draw UI
 
         ImGui::Text("connect %s (%s) to:", socket->getName().c_str(), socket->getConnecteeTypeName().c_str());
 
@@ -59,26 +160,19 @@ private:
         ImGui::Separator();
         ImGui::Dummy({0.0f, 0.25f * ImGui::GetTextLineHeight()});
 
-        osc::DrawSearchBar(m_Search, 256);
+        osc::DrawSearchBar(m_EditedParams.search, 256);
 
-        OpenSim::Component const* chosenComponent = nullptr;
-        int id = 0;
+        std::optional<OpenSim::ComponentPath> userSelection;
+        int id = 0;  // care: necessary because multiple connectees may have the same name
         ImGui::BeginChild("##componentlist", ImVec2(512, 256), true, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_AlwaysVerticalScrollbar);
-        for (OpenSim::Component const& possibleConnectee : m_Model->getModel().getComponentList())
+        for (ConnecteeOption const& option : m_Options)
         {
-            std::string const& name = possibleConnectee.getName();
-
-            if (&possibleConnectee != component &&
-                ContainsSubstring(name, m_Search) &&
-                IsAbleToConnectTo(*socket, possibleConnectee))
+            ImGui::PushID(id++);
+            if (ImGui::Selectable(option.name.c_str()))
             {
-                ImGui::PushID(id++);
-                if (ImGui::Selectable(name.c_str()))
-                {
-                    chosenComponent = &possibleConnectee;
-                }
-                ImGui::PopID();
+                userSelection = option.absPath;
             }
+            ImGui::PopID();
         }
         ImGui::EndChild();
 
@@ -94,23 +188,28 @@ private:
             return;
         }
 
-        if (chosenComponent && osc::ActionReassignSelectedComponentSocket(*m_Model, m_ComponentPath, m_SocketName, *chosenComponent, m_Error))
+        // if the user selected something, try to form the connection in the active model
+        if (userSelection)
         {
-            requestClose();
-            return;
+            OpenSim::Component const* selected = osc::FindComponent(m_Model->getModel(), *userSelection);
+            if (selected && osc::ActionReassignSelectedComponentSocket(*m_Model, m_Params.path, m_Params.socketName, *selected, m_Error))
+            {
+                requestClose();
+                return;
+            }
         }
     }
 
     void implOnClose() override
     {
-        m_Search.clear();
+        m_EditedParams.search.clear();
         m_Error.clear();
     }
 
     std::shared_ptr<UndoableModelStatePair> m_Model;
-    OpenSim::ComponentPath m_ComponentPath;
-    std::string m_SocketName;
-    std::string m_Search;
+    PopupParams m_Params;
+    PopupParams m_EditedParams = m_Params;
+    std::vector<ConnecteeOption> m_Options = GenerateSelectionOptions(m_Model->getModel(), m_EditedParams);
     std::string m_Error;
 };
 
