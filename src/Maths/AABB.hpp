29f827c2f5abc5120564074aca06b1d0a20b6a50
Adam Kewley <contact@adamkewley.com>
2022-10-11 14:30:10 +0200

    Re-added operator== and operator!= for osc::AABB

--- src/Maths/AABB.hpp
+++ src/Maths/AABB.hpp
@@ -12,4 +12,6 @@ namespace osc
     };
 
     std::ostream& operator<<(std::ostream&, AABB const&);
+    bool operator==(AABB const&, AABB const&) noexcept;
+    bool operator!=(AABB const&, AABB const&) noexcept;
 }
--- src/Maths/MathsImplementation.cpp
+++ src/Maths/MathsImplementation.cpp
@@ -49,6 +49,16 @@ std::ostream& osc::operator<<(std::ostream& o, AABB const& aabb)
     return o << "AABB(min = " << aabb.min << ", max = " << aabb.max << ')';
 }
 
+bool osc::operator==(AABB const& a, AABB const& b) noexcept
+{
+    return a.min == b.min && a.max == b.max;
+}
+
+bool osc::operator!=(AABB const& a, AABB const& b) noexcept
+{
+    return !(a == b);
+}
+
 
 // BVH implementation
 
