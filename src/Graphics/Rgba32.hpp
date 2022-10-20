e32835deac9073aee3308cc7c47ee94bb3f94564
Adam Kewley <contact@adamkewley.com>
2022-10-20 10:46:01 +0200

    Added utility functions to osc::Rgba32

--- src/Graphics/Rgba32.hpp
+++ src/Graphics/Rgba32.hpp
@@ -1,13 +1,32 @@
 #pragma once
 
 #include <cstdint>
+#include <functional>
 
 namespace osc
 {
-    struct Rgba32 final {
+    struct alignas(alignof(uint32_t)) Rgba32 final {
         uint8_t r;
         uint8_t g;
         uint8_t b;
         uint8_t a;
+    };
+
+    inline uint32_t ToUint32(Rgba32 const& rgba32)
+    {
+        static_assert(alignof(Rgba32) == alignof(uint32_t));
+        static_assert(sizeof(Rgba32) == sizeof(uint32_t));
+        return reinterpret_cast<uint32_t const&>(rgba32);
+    }
+}
+
+namespace std
+{
+    template<>
+    struct hash<osc::Rgba32> final {
+        size_t operator()(osc::Rgba32 const& rgba32) const noexcept
+        {
+            return std::hash<uint32_t>{}(ToUint32(rgba32));
+        }
     };
 }
