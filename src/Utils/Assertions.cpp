8de87f9612ffd2cbb6bf948ab5d93543fdefe806
Adam Kewley <contact@adamkewley.com>
2022-11-15 13:44:01 +0100

    Made assertion buffer threadsafe and hardened against init-order errors

--- src/Utils/Assertions.cpp
+++ src/Utils/Assertions.cpp
@@ -1,24 +1,35 @@
 #include "Assertions.hpp"
 
 #include "src/Platform/Log.hpp"
+#include "src/Utils/SynchronizedValue.hpp"
 
 #include <array>
 #include <cstdio>
 #include <stdexcept>
 
-// allocate this globally and ahead of time, because:
-//
-// - using the memory allocator during an assertion failure might trigger other problems
-// - stack-allocating a large buffer can cause other issues (you don't know how much stack space you have)
-static std::array<char, 2048> g_MessageBuffer{};
+namespace
+{
+    // returns a static, global, buffer that assertion error messages can be formatted into
+    //
+    // the reason this is necessary is because we want to avoid memory allocations during
+    // assertion errors
+    osc::SynchronizedValue<std::array<char, 2048>>& GetGlobalAssertionErrorBuffer()
+    {
+        static osc::SynchronizedValue<std::array<char, 2048>> s_MessageBuffer{};
+        return s_MessageBuffer;
+    }
+}
 
 void osc::OnAssertionFailure(char const* failing_code,
                              char const* func,
                              char const* file,
                              unsigned int line) noexcept
 {
-    std::snprintf(g_MessageBuffer.data(), g_MessageBuffer.size(), "%s:%s:%u: assert(%s): failed", file, func, line, failing_code);
-    log::error("%s", g_MessageBuffer.data());
+    auto& buf = GetGlobalAssertionErrorBuffer();
+    auto guard = buf.lock();
+
+    std::snprintf(guard->data(), guard->size(), "%s:%s:%u: assert(%s): failed", file, func, line, failing_code);
+    log::error("%s", guard->data());
     std::terminate();
 }
 
@@ -28,7 +39,10 @@ void osc::OnThrowingAssertionFailure(
     char const* file,
     unsigned int line)
 {
-    std::snprintf(g_MessageBuffer.data(), g_MessageBuffer.size(), "%s:%s:%u: throw_if_not(%s): failed", file, func, line, failingCode);
-    log::error("%s", g_MessageBuffer.data());
-    throw std::runtime_error{g_MessageBuffer.data()};
+    auto& buf = GetGlobalAssertionErrorBuffer();
+    auto guard = buf.lock();
+
+    std::snprintf(guard->data(), guard->size(), "%s:%s:%u: throw_if_not(%s): failed", file, func, line, failingCode);
+    log::error("%s", guard->data());
+    throw std::runtime_error{guard->data()};
 }
