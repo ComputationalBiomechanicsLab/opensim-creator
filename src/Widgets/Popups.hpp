993b351d61f7ac28374c30eaf7249cc050572c5d
Adam Kewley <contact@adamkewley.com>
2022-12-07 13:53:18 +0100

    Add missing elements in osc::Popups (#467)

--- src/Widgets/Popups.hpp
+++ src/Widgets/Popups.hpp
@@ -10,6 +10,14 @@ namespace osc
     // generic storage for a drawable popup stack
     class Popups final {
     public:
+        Popups();
+        Popups(Popups const&) = delete;
+        Popups(Popups&&) noexcept;
+        Popups& operator=(Popups const&) = delete;
+        Popups& operator=(Popups&&) noexcept;
+        ~Popups() noexcept;
+
+        void push_back(std::unique_ptr<Popup>);
         void draw();
     private:
         std::vector<std::unique_ptr<Popup>> m_Popups;
