42d012f0af78b8a634e6ca90b3bc2b34ccddf7e2
Adam Kewley <contact@adamkewley.com>
2023-03-08 12:53:00 +0100

    Added failing test repro for #642

---
+++ tests/TestOpenSimActions.cpp
@@ -0,0 +1,22 @@
+#include "osc_test_config.hpp"
+
+#include "src/OpenSimBindings/ActionFunctions.hpp"
+#include "src/OpenSimBindings/UndoableModelStatePair.hpp"
+
+#include <gtest/gtest.h>
+#include <OpenSim/Simulation/Model/Model.h>
+
+// repro for #642
+//
+// user reported that trying to add a body with an invalid name entirely crashes
+// OSC, which implies that the operation causes a segfault
+TEST(OpenSimActions, ActionAddBodyToModelThrowsIfBodyNameIsInvalid)
+{
+	osc::UndoableModelStatePair model;
+
+    osc::BodyDetails details;
+    details.bodyName = "test 1";
+    details.parentFrameAbsPath = "/ground";  // this is what the dialog defaults to
+
+    osc::ActionAddBodyToModel(model, details);
+}
