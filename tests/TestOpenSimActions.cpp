8cad8ae2173c5cde9e38b9d6e3acf19dc11eb1b5
Adam Kewley <contact@adamkewley.com>
2023-03-21 10:05:41 +0100

    Added repro test for #495

--- tests/TestOpenSimActions.cpp
+++ tests/TestOpenSimActions.cpp
@@ -20,3 +20,23 @@ TEST(OpenSimActions, ActionAddBodyToModelThrowsIfBodyNameIsInvalid)
 
     ASSERT_ANY_THROW({ osc::ActionAddBodyToModel(model, details); });
 }
+
+// repro for #495
+//
+// @JuliaVanBeesel reported that, when editing an OpenSim model via the editor UI, if
+// they then delete the backing file (e.g. via Windows explorer), the editor UI will
+// then show an error message from an exception, rather than carrying on or warning
+// that something not-quite-right has happened
+TEST(OpenSimActions, ActionUpdateModelFromBackingFileReturnsFalseIfFileDoesNotExist)
+{
+    osc::UndoableModelStatePair model;
+
+    // it just returns `false` if there's no backing file
+    ASSERT_FALSE(osc::ActionUpdateModelFromBackingFile(model));
+
+    // ... but if you say it has an invalid backing file path...
+    model.setFilesystemPath("doesnt-exist");
+
+    // then it should just return `false`, rather than (e.g.) exploding
+    ASSERT_FALSE(osc::ActionUpdateModelFromBackingFile(model));
+}
