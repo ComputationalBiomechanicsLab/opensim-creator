8ed67b8c67092c8c3e5fdd964ce2b63c5c48ae49
Adam Kewley <contact@adamkewley.com>
2023-06-09 19:29:59 +0200

    Begin to flesh out point shadows democode

--- src/oscar/Tabs/LearnOpenGL/LOGLPointShadowsTab.cpp
+++ src/oscar/Tabs/LearnOpenGL/LOGLPointShadowsTab.cpp
@@ -1,16 +1,78 @@
 #include "LOGLPointShadowsTab.hpp"
 
+#include "oscar/Bindings/ImGuiHelpers.hpp"
+#include "oscar/Graphics/Camera.hpp"
+#include "oscar/Graphics/ColorSpace.hpp"
+#include "oscar/Graphics/GraphicsHelpers.hpp"
+#include "oscar/Graphics/Mesh.hpp"
+#include "oscar/Graphics/MeshGen.hpp"
+#include "oscar/Graphics/RenderTexture.hpp"
+#include "oscar/Graphics/RenderTextureDescriptor.hpp"
+#include "oscar/Graphics/RenderTextureReadWrite.hpp"
+#include "oscar/Graphics/Texture2D.hpp"
+#include "oscar/Maths/Transform.hpp"
+#include "oscar/Platform/App.hpp"
+#include "oscar/Utils/Algorithms.hpp"
 #include "oscar/Utils/CStringView.hpp"
+#include "oscar/Utils/UID.hpp"
 
+#include <glm/vec3.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <IconsFontAwesome5.h>
 #include <SDL_events.h>
 
+#include <chrono>
+#include <cmath>
 #include <string>
 #include <utility>
 
 namespace
 {
+    constexpr glm::ivec2 c_ShadowmapDims = {1024, 1024};
     constexpr osc::CStringView c_TabStringID = "LearnOpenGL/PointShadows";
+
+    constexpr osc::Transform MakeTransform(float scale, glm::vec3 position)
+    {
+        osc::Transform rv;
+        rv.scale = glm::vec3(scale);
+        rv.position = position;
+        return rv;
+    }
+
+    osc::Transform MakeRotatedTransform()
+    {
+        osc::Transform rv;
+        rv.scale = glm::vec3(0.75f);
+        rv.rotation = glm::angleAxis(glm::radians(60.0f), glm::normalize(glm::vec3{1.0f, 0.0f, 1.0f}));
+        rv.position = {-1.5f, 2.0f, -3.0f};
+        return rv;
+    }
+
+    auto const c_CubeTransforms = osc::MakeArray<osc::Transform>(
+        MakeTransform(0.5f, {4.0f, -3.5f, 0.0f}),
+        MakeTransform(0.75f, {2.0f, 3.0f, 1.0f}),
+        MakeTransform(0.5f, {-3.0f, -1.0f, 0.0f}),
+        MakeTransform(0.5f, {-1.5f, 1.0f, 1.5f}),
+        MakeRotatedTransform()
+    );
+
+    osc::Camera CreateCamera()
+    {
+        osc::Camera rv;
+        rv.setBackgroundColor({0.1f, 0.1f, 0.1f, 1.0f});
+        rv.setPosition({0.0f, 0.0f, 3.0f});
+        rv.setNearClippingPlane(0.1f);
+        rv.setFarClippingPlane(100.0f);
+        return rv;
+    }
+
+    osc::RenderTexture CreateDepthTexture()
+    {
+        // TODO: this needs to be a cubemap
+        osc::RenderTextureDescriptor desc{c_ShadowmapDims};
+        desc.setReadWrite(osc::RenderTextureReadWrite::Linear);
+        return osc::RenderTexture{std::move(desc)};
+    }
 }
 
 class osc::LOGLPointShadowsTab::Impl final {
@@ -33,19 +95,37 @@ public:
 
     void onMount()
     {
+        App::upd().makeMainEventLoopPolling();
+        m_IsMouseCaptured = true;
     }
 
     void onUnmount()
     {
+        m_IsMouseCaptured = false;
+        App::upd().makeMainEventLoopWaiting();
+        App::upd().setShowCursor(true);
     }
 
-    bool onEvent(SDL_Event const&)
+    bool onEvent(SDL_Event const& e)
     {
+        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)
+        {
+            m_IsMouseCaptured = false;
+            return true;
+        }
+        else if (e.type == SDL_MOUSEBUTTONDOWN && osc::IsMouseInMainViewportWorkspaceScreenRect())
+        {
+            m_IsMouseCaptured = true;
+            return true;
+        }
         return false;
     }
 
     void onTick()
     {
+        // move light position over time
+        float const seconds = App::get().getDeltaSinceAppStartup().count();
+        m_LightPos.z = 3.0f * std::sin(0.5f * seconds);
     }
 
     void onDrawMainMenu()
@@ -54,11 +134,44 @@ public:
 
     void onDraw()
     {
+        handleMouseCapture();
+        draw3DScene();
     }
 
 private:
+    void handleMouseCapture()
+    {
+        if (m_IsMouseCaptured)
+        {
+            UpdateEulerCameraFromImGuiUserInput(m_Camera, m_CameraEulers);
+            ImGui::SetMouseCursor(ImGuiMouseCursor_None);
+            App::upd().setShowCursor(false);
+        }
+        else
+        {
+            ImGui::SetMouseCursor(ImGuiMouseCursor_Arrow);
+            App::upd().setShowCursor(true);
+        }
+    }
+
+    void draw3DScene()
+    {
+        // TODO
+    }
+
     UID m_TabID;
     std::weak_ptr<TabHost> m_Parent;
+
+    Camera m_Camera = CreateCamera();
+    bool m_IsMouseCaptured = false;
+    glm::vec3 m_CameraEulers = {0.0f, 0.0f, 0.0f};
+    Texture2D m_WoodTexture = LoadTexture2DFromImage(
+        App::resource("textures/wood.png"),
+        ColorSpace::sRGB
+    );
+    Mesh m_CubeMesh = GenCube();
+    RenderTexture m_DepthTexture = CreateDepthTexture();
+    glm::vec3 m_LightPos = {0.0f, 0.0f, 0.0f};
 };
 
 
