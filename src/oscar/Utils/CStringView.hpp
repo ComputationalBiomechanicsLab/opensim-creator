c1ff713620418e0459a5e89b3b61e87445f96b95
Adam Kewley <contact@adamkewley.com>
2023-07-13 20:40:56 +0200

    Add changes for readability- lint

--- src/oscar/Utils/CStringView.hpp
+++ src/oscar/Utils/CStringView.hpp
@@ -30,11 +30,17 @@ namespace osc
     public:
         constexpr CStringView() noexcept : m_Data{nullptr}, m_Size{0} {}
         constexpr CStringView(CStringView const&) noexcept = default;
-        constexpr CStringView(char const* s) : m_Data{s}, m_Size{StrLen(s)} {}
-        constexpr CStringView(std::nullptr_t) : CStringView{} {}
-        template<size_t N> constexpr CStringView(char const s[N]) : m_Data{s}, m_Size{N} {}
-        CStringView(std::string const& s) : m_Data{s.c_str()}, m_Size{s.size()} {}
+        constexpr CStringView(char const* s) noexcept : m_Data{s}, m_Size{StrLen(s)} {}
+        constexpr CStringView(std::nullptr_t) noexcept : CStringView{} {}
+        CStringView(std::string const& s) noexcept : m_Data{s.c_str()}, m_Size{s.size()} {}
         constexpr CStringView& operator=(CStringView const&) noexcept = default;
+
+        // explicitly necessary because arrays might otherwise decay into pointers
+	template<size_t N>
+        static constexpr CStringView FromArray(char const (&s)[N]) noexcept
+	{
+            return {&s[0], N};
+	}
 
         constexpr size_t size() const noexcept { return m_Size; }
         constexpr size_t length() const noexcept { return m_Size; }
@@ -45,6 +51,8 @@ namespace osc
         constexpr char const* end() const noexcept { return m_Data + m_Size; }
 
     private:
+        constexpr CStringView(char const* data_, size_t size_) noexcept : m_Data{data_}, m_Size{size_} {}
+
         friend bool operator<(CStringView const&, CStringView const&);
         friend bool operator==(CStringView const&, CStringView const&);
         friend bool operator!=(CStringView const&, CStringView const&);
