3ce7c1dcb1fd6730169513573c7a8ef7adcab4ac
Adam Kewley <contact@adamkewley.com>
2023-07-24 15:39:46 +0200

    Reformat tabs --> spaces

--- src/OpenSimCreator/Widgets/ExportPointsPopup.hpp
+++ src/OpenSimCreator/Widgets/ExportPointsPopup.hpp
@@ -9,27 +9,27 @@ namespace osc { class VirtualConstModelStatePair; }
 
 namespace osc
 {
-	class ExportPointsPopup final : public Popup {
-	public:
-		ExportPointsPopup(
-			std::string_view popupName,
-			std::shared_ptr<VirtualConstModelStatePair const>
-		);
-		ExportPointsPopup(ExportPointsPopup const&) = delete;
-		ExportPointsPopup(ExportPointsPopup&&) noexcept;
-		ExportPointsPopup& operator=(ExportPointsPopup const&) = delete;
-		ExportPointsPopup& operator=(ExportPointsPopup&&) noexcept;
-		~ExportPointsPopup() noexcept;
+    class ExportPointsPopup final : public Popup {
+    public:
+        ExportPointsPopup(
+            std::string_view popupName,
+            std::shared_ptr<VirtualConstModelStatePair const>
+        );
+        ExportPointsPopup(ExportPointsPopup const&) = delete;
+        ExportPointsPopup(ExportPointsPopup&&) noexcept;
+        ExportPointsPopup& operator=(ExportPointsPopup const&) = delete;
+        ExportPointsPopup& operator=(ExportPointsPopup&&) noexcept;
+        ~ExportPointsPopup() noexcept;
 
-	private:
-		bool implIsOpen() const final;
-		void implOpen() final;
-		void implClose() final;
-		bool implBeginPopup() final;
-		void implDrawPopupContent() final;
-		void implEndPopup() final;
+    private:
+        bool implIsOpen() const final;
+        void implOpen() final;
+        void implClose() final;
+        bool implBeginPopup() final;
+        void implDrawPopupContent() final;
+        void implEndPopup() final;
 
-		class Impl;
-		std::unique_ptr<Impl> m_Impl;
-	};
+        class Impl;
+        std::unique_ptr<Impl> m_Impl;
+    };
 }
