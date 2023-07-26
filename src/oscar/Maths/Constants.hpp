ff0de72cf8b555410b0274833593e5bc5940bc13
Adam Kewley <contact@adamkewley.com>
2023-07-26 11:55:21 +0200

    Remove M_PI definition

--- src/oscar/Maths/Constants.hpp
+++ src/oscar/Maths/Constants.hpp
@@ -1,12 +1,8 @@
 #pragma once
 
-#ifndef M_PI
-    #define M_PI 3.14159265358979323846
-#endif
-
 namespace osc
 {
-    static inline constexpr float fpi = static_cast<float>(M_PI);
+    static inline constexpr float fpi = static_cast<float>(3.14159265358979323846);
     static inline constexpr float fpi2 = fpi/2.0f;
     static inline constexpr float fpi4 = fpi/4.0f;
     static inline constexpr float f1pi = 1.0f/fpi;
