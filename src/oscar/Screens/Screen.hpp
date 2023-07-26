c9752a87a81042f4b36309f031cb3a2c4e4f688f
Adam Kewley <contact@adamkewley.com>
2023-07-26 13:57:37 +0200

    Minor style refactor

--- src/oscar/Screens/Screen.hpp
+++ src/oscar/Screens/Screen.hpp
@@ -62,24 +62,16 @@ namespace osc
         }
 
         // called before the app is about to start pump-/tick-/draw-ing the screen
-        virtual void implOnMount()
-        {
-        }
+        virtual void implOnMount() {}
 
         // called after the last time the app pumps/ticks/draws the screen
-        virtual void implOnUnmount()
-        {
-        }
+        virtual void implOnUnmount() {}
 
         // called by app to pump an event to the screen
-        virtual void implOnEvent(SDL_Event const&)
-        {
-        }
+        virtual void implOnEvent(SDL_Event const&) {}
 
         // called by app once per frame (float is a timedelta in seconds)
-        virtual void implOnTick()
-        {
-        }
+        virtual void implOnTick() {}
 
         // called by app when the screen should render into the current framebuffer
         virtual void implOnDraw() = 0;
