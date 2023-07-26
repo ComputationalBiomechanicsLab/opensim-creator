af076026bcde62b3fa9913276474f637ac7369ec
Adam Kewley <contact@adamkewley.com>
2023-07-26 14:03:09 +0200

    Refactor cleanup Tab.{hpp,cpp}

--- src/oscar/Tabs/Tab.hpp
+++ src/oscar/Tabs/Tab.hpp
@@ -19,44 +19,27 @@ namespace osc
     public:
         virtual ~Tab() noexcept = default;
 
-        UID getID() const;
-        CStringView getName() const;
-        bool isUnsaved() const;
-        bool trySave();
-        void onMount();
-        void onUnmount();
-        bool onEvent(SDL_Event const& e);
-        void onTick();
-        void onDrawMainMenu();
-        void onDraw();
+        UID getID() const { return implGetID(); }
+        CStringView getName() const { return implGetName(); }
+        bool isUnsaved() const { return implIsUnsaved(); }
+        bool trySave() { return implTrySave(); }
+        void onMount() { implOnMount(); }
+        void onUnmount() { implOnUnmount(); }
+        bool onEvent(SDL_Event const& e) { return implOnEvent(e); }
+        void onTick() { implOnTick(); }
+        void onDrawMainMenu() { implOnDrawMainMenu(); }
+        void onDraw() { implOnDraw(); }
 
     private:
         virtual UID implGetID() const = 0;
         virtual CStringView implGetName() const = 0;
-        virtual bool implIsUnsaved() const
-        {
-            return false;
-        }
-        virtual bool implTrySave()
-        {
-            return true;
-        }
-        virtual void implOnMount()
-        {
-        }
-        virtual void implOnUnmount()
-        {
-        }
-        virtual bool implOnEvent(SDL_Event const&)
-        {
-            return false;
-        }
-        virtual void implOnTick()
-        {
-        }
-        virtual void implOnDrawMainMenu()
-        {
-        }
+        virtual bool implIsUnsaved() const { return false; }
+        virtual bool implTrySave() { return true; }
+        virtual void implOnMount() {}
+        virtual void implOnUnmount() {}
+        virtual bool implOnEvent(SDL_Event const&) { return false;}
+        virtual void implOnTick() {}
+        virtual void implOnDrawMainMenu() {}
         virtual void implOnDraw() = 0;
     };
 }
