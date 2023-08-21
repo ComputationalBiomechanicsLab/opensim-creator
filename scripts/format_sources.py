c8c24f09d10717f7f99277ec70a56c5c68a3c6b8
Adam Kewley <contact@adamkewley.com>
2023-08-21 12:03:11 +0200

    Merge scripts/formatters/ into scripts/format_sources.py

---
+++ scripts/format_sources.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+# recursively adds trailing newline to any files missing one in the given dir
+
+import argparse
+import os
+import re
+
+def num_leading_tabs(s):
+    rv = 0
+    for c in s:
+        if c == '\t':
+            rv += 1
+        else:
+            break
+    return rv
+
+def read_lines(path):
+    with open(path) as f:
+        return f.read().splitlines()
+
+def replace_leading_tabs(line):
+    n = num_leading_tabs(line)
+    if n:
+        return (n*'    ' + line[n:], True)
+    else:
+        return (line, False)
+
+# recursively tries to convert any tabs to spaces in the source tree
+#
+# (tabs sometimes slip into the source tree from editing files in terminals)
+def replace_leading_tabs_with_spaces_in(dirpath):
+    for root, subdirs, files in os.walk(dirpath):
+        for file in files:
+            path = os.path.join(root, file)
+            lines = read_lines(path)
+
+            should_write = False
+            new_lines = []
+            for line in lines:
+                content, changed = replace_leading_tabs(line)
+                new_lines += [content]
+                should_write = should_write or changed
+
+            if should_write:
+                new_content = "\n".join(new_lines) + "\n"  # trailing newline
+                with open(path, "w") as f:
+                    f.write(new_content)
+
+# recursively tries to strip whitespace from all files in a source
+# tree
+#
+# (whitespace sometimes slips in from various text editors etc.)
+def strip_whitespace_for_all_files_in(dirname):
+    pattern  = re.compile(r"[ \t]+$", re.MULTILINE)
+    for root, subdirs, files in os.walk(dirname):
+        for file in files:
+            p = os.path.join(root, file)
+
+            new_content = None
+            with open(p) as f:
+                content = f.read()
+                match = re.search(pattern, content)
+                if match:
+                    new_content = re.sub(pattern, "", content)
+            if new_content:
+                with open(p, "w") as f:
+                    f.write(new_content)
+
+# recursively adds trailing newline to any files missing one in the given dir
+def ensure_all_files_have_trailing_newline(dirpath):
+    for root, subdirs, files in os.walk(dirpath):
+        for file in files:
+            path = os.path.join(root, file)
+            with open(path, "r") as f:
+                content = f.read()
+            if not content.endswith("\n"):
+                with open(path, "w") as f:
+                    f.write(content + "\n")
+
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("dirpath")
+    parsed = p.parse_args()
+    ensure_all_files_have_trailing_newline(parsed.dirpath)
+    strip_whitespace_for_all_files_in(parsed.dirpath)
+    replace_leading_tabs_with_spaces_in(parsed.dirpath)
+
+if __name__ == '__main__':
+    main()
