4ce8349252b3afa99c71cbb66c42afb19fbe718f
Adam Kewley <contact@adamkewley.com>
2023-08-23 12:56:58 +0200

    Add UI-command-level repro test for coordinate range deletion bug (#654)

--- tests/OpenSimCreator/Utils/TestUndoableModelActions.cpp
+++ tests/OpenSimCreator/Utils/TestUndoableModelActions.cpp
@@ -1,10 +1,18 @@
 #include "OpenSimCreator/Utils/UndoableModelActions.hpp"
 
+#include "OpenSimCreator/Model/ObjectPropertyEdit.hpp"
 #include "OpenSimCreator/Model/UndoableModelStatePair.hpp"
 #include "testopensimcreator_config.hpp"
 
 #include <gtest/gtest.h>
+#include <OpenSim/Common/AbstractProperty.h>
 #include <OpenSim/Simulation/Model/Model.h>
+#include <OpenSim/Simulation/SimbodyEngine/Body.h>
+#include <OpenSim/Simulation/SimbodyEngine/Coordinate.h>
+#include <OpenSim/Simulation/SimbodyEngine/PinJoint.h>
+
+#include <functional>
+#include <memory>
 
 // repro for #642
 //
@@ -39,4 +47,39 @@ TEST(OpenSimActions, ActionUpdateModelFromBackingFileReturnsFalseIfFileDoesNotEx
 
     // then it should just return `false`, rather than (e.g.) exploding
     ASSERT_FALSE(osc::ActionUpdateModelFromBackingFile(model));
+}
+
+// repro for #654
+//
+// the bug is in OpenSim, but the action needs to hack around that bug until it is fixed
+// upstream
+TEST(OpenSimActions, ActionApplyRangeDeletionPropertyEditShouldThrow)
+{
+    // create undoable model with one body + joint
+    auto undoableModel = []()
+    {
+        OpenSim::Model model;
+        auto body = std::make_unique<OpenSim::Body>("body", 1.0, SimTK::Vec3{}, SimTK::Inertia{});
+        auto joint = std::make_unique<OpenSim::PinJoint>();
+        joint->setName("joint");
+        joint->updCoordinate().setName("rotation");
+        joint->connectSocket_parent_frame(model.getGround());
+        joint->connectSocket_child_frame(*body);
+        model.addJoint(joint.release());
+        model.addBody(body.release());
+        model.finalizeConnections();
+        return osc::UndoableModelStatePair{std::make_unique<OpenSim::Model>(std::move(model))};
+    }();
+
+    osc::ObjectPropertyEdit edit
+    {
+        undoableModel.updModel().updComponent<OpenSim::Coordinate>("/jointset/joint/rotation").updProperty_range(),
+        [](OpenSim::AbstractProperty& p)
+        {
+            p.clear();
+        },
+    };
+
+    // should throw on application of the faulty edit, not at some later time
+    ASSERT_ANY_THROW({ osc::ActionApplyPropertyEdit(undoableModel, edit); });
 }
