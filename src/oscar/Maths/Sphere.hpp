7e73f4446941085c537ae1e04065b71447a62890
Adam Kewley <contact@adamkewley.com>
2023-10-31 13:56:18 +0100

    Add default ctor to osc::Ellipsoid and osc::Sphere

--- src/oscar/Maths/Sphere.hpp
+++ src/oscar/Maths/Sphere.hpp
@@ -7,8 +7,8 @@
 namespace osc
 {
     struct Sphere final {
-        glm::vec3 origin;
-        float radius;
+        glm::vec3 origin = {};
+        float radius = 1.0f;
     };
 
     std::ostream& operator<<(std::ostream&, Sphere const&);
