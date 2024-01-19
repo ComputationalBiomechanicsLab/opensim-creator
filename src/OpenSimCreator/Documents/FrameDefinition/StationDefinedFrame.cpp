7adb5eea1c560f06327814296dbfe5349f6e8406
Adam Kewley <contact@adamkewley.com>
2024-01-19 10:20:14 +0100

    Stub out StationDefinedFrame impl.

--- src/OpenSimCreator/Documents/FrameDefinition/StationDefinedFrame.cpp
+++ src/OpenSimCreator/Documents/FrameDefinition/StationDefinedFrame.cpp
@@ -1,1 +1,53 @@
 #include "StationDefinedFrame.hpp"
+
+
+osc::fd::StationDefinedFrame::StationDefinedFrame()
+{
+    constructProperty_ab_axis("+x");
+    constructProperty_ab_x_ac_axis("+y");
+}
+
+const OpenSim::Frame& osc::fd::StationDefinedFrame::extendFindBaseFrame() const
+{
+    return getConnectee<OpenSim::Station>("point_a").getParentFrame().findBaseFrame();
+}
+
+SimTK::Transform osc::fd::StationDefinedFrame::extendFindTransformInBaseFrame() const
+{
+    // TODO:
+    //
+    // - compute transform from stations (put everything into the base frame)
+    return {};  // TODO
+}
+
+void osc::fd::StationDefinedFrame::extendFinalizeFromProperties()
+{
+    // TODO: validate orthogonality of axis designations
+}
+
+void osc::fd::StationDefinedFrame::extendConnectToModel(OpenSim::Model&)
+{
+    // TODO: validate all three stations exist and are defined in the same base frame
+}
+
+SimTK::Transform osc::fd::StationDefinedFrame::calcTransformInGround(const SimTK::State&) const
+{
+    // TODO: get base frame's transform in ground and compose it with `getTransformInBaseFrame`
+    return {};
+}
+
+SimTK::SpatialVec osc::fd::StationDefinedFrame::calcVelocityInGround(const SimTK::State&) const
+{
+    // TODO: get base frame's velocity in ground and compose it with `getTransformInBaseFrame`
+    //
+    // see: `OpenSim::OffsetFrame<T>::calcVelocityInGround` for calculation
+    return {};
+}
+
+SimTK::SpatialVec osc::fd::StationDefinedFrame::calcAccelerationInGround(const SimTK::State&) const
+{
+    // TODO: get base frame's acceleration in ground and compose it with `getTransformInBaseFrame`
+    //
+    // see: `OpenSim::OffsetFrame<T>::calcAccelerationInGround` for calculation
+    return {};
+}
--- tests/TestOpenSimCreator/Documents/FrameDefinition/TestStationDefinedFrame.cpp
+++ tests/TestOpenSimCreator/Documents/FrameDefinition/TestStationDefinedFrame.cpp
@@ -1,1 +1,10 @@
+#include "OpenSimCreator/Documents/FrameDefinition/StationDefinedFrame.hpp"
 
+#include <gtest/gtest.h>
+
+using osc::fd::StationDefinedFrame;
+
+TEST(StationDefinedFrame, CanBeDefaultConstructed)
+{
+    ASSERT_NO_THROW({ StationDefinedFrame{}; });
+}
