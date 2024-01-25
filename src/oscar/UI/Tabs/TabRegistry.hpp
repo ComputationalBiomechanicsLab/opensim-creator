31b0266fd54a4c3db051b6cf064cde9e600e0d47
Adam Kewley <contact@adamkewley.com>
2024-01-25 12:49:36 +0100

    Refactor LearnOpenGLTabRegistry registration approach

--- src/oscar/UI/Tabs/TabRegistry.hpp
+++ src/oscar/UI/Tabs/TabRegistry.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <oscar/UI/Tabs/ITab.hpp>
+#include <oscar/UI/Tabs/ITabHost.hpp>
 #include <oscar/UI/Tabs/TabRegistryEntry.hpp>
+#include <oscar/Utils/CStringView.hpp>
+#include <oscar/Utils/ParentPtr.hpp>
 
+#include <concepts>
 #include <cstddef>
 #include <memory>
 #include <optional>
@@ -9,6 +14,14 @@
 
 namespace osc
 {
+    template<typename T>
+    concept StandardRegisterableTab =
+        std::derived_from<T, ITab> &&
+        std::constructible_from<T, ParentPtr<ITabHost> const&> &&
+        requires (T) {
+            { T::id() } -> std::same_as<CStringView>;
+        };
+
     // container for alphabetically-sorted tab entries
     class TabRegistry final {
     public:
@@ -20,6 +33,16 @@ namespace osc
         ~TabRegistry() noexcept;
 
         void registerTab(TabRegistryEntry const&);
+
+        template<StandardRegisterableTab T>
+        void registerTab()
+        {
+            registerTab(TabRegistryEntry{
+                T::id(),
+                [](ParentPtr<ITabHost> const& h) { return std::make_unique<T>(h); },
+            });
+        }
+
         size_t size() const;
         TabRegistryEntry operator[](size_t) const;
         std::optional<TabRegistryEntry> getByName(std::string_view) const;
