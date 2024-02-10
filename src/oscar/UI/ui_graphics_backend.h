afd2a8a3050e6275764e2c2d61e9b3fd8a279137
Adam Kewley <contact@adamkewley.com>
2024-02-10 12:30:09 +0100

    Fix bad .gitignore settings for ui_.h files

---
+++ src/oscar/UI/ui_graphics_backend.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <imgui.h>
+
+namespace osc { class RenderTexture; }
+namespace osc { class Texture2D; }
+
+// ImGui graphics backend that uses osc's graphics API
+struct ImDrawData;
+
+namespace osc::ui::graphics_backend
+{
+    bool Init();
+    void Shutdown();
+    void NewFrame();
+    void RenderDrawData(ImDrawData*);
+    ImTextureID AllocateTextureID(Texture2D const&);
+    ImTextureID AllocateTextureID(RenderTexture const&);
+}
