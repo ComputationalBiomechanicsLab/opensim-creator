9cbe09cd0eb5455212b6afed5a499a58f9029925
Adam Kewley <contact@adamkewley.com>
2024-02-12 13:34:49 +0100

    Add hierarchical headers to oscar_learnopengl

---
+++ src/oscar_learnopengl/AdvancedOpenGL.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <oscar_learnopengl/AdvancedOpenGL/LOGLBlendingTab.h>
+#include <oscar_learnopengl/AdvancedOpenGL/LOGLCubemapsTab.h>
+#include <oscar_learnopengl/AdvancedOpenGL/LOGLFaceCullingTab.h>
+#include <oscar_learnopengl/AdvancedOpenGL/LOGLFramebuffersTab.h>
--- src/oscar_learnopengl/LearnOpenGLTabs.h
+++ src/oscar_learnopengl/LearnOpenGLTabs.h
@@ -1,29 +1,10 @@
 #pragma once
 
-#include <oscar_learnopengl/AdvancedLighting/LOGLBloomTab.h>
-#include <oscar_learnopengl/AdvancedLighting/LOGLDeferredShadingTab.h>
-#include <oscar_learnopengl/AdvancedLighting/LOGLGammaTab.h>
-#include <oscar_learnopengl/AdvancedLighting/LOGLHDRTab.h>
-#include <oscar_learnopengl/AdvancedLighting/LOGLNormalMappingTab.h>
-#include <oscar_learnopengl/AdvancedLighting/LOGLParallaxMappingTab.h>
-#include <oscar_learnopengl/AdvancedLighting/LOGLPointShadowsTab.h>
-#include <oscar_learnopengl/AdvancedLighting/LOGLSSAOTab.h>
-#include <oscar_learnopengl/AdvancedLighting/LOGLShadowMappingTab.h>
-#include <oscar_learnopengl/AdvancedOpenGL/LOGLBlendingTab.h>
-#include <oscar_learnopengl/AdvancedOpenGL/LOGLCubemapsTab.h>
-#include <oscar_learnopengl/AdvancedOpenGL/LOGLFaceCullingTab.h>
-#include <oscar_learnopengl/AdvancedOpenGL/LOGLFramebuffersTab.h>
-#include <oscar_learnopengl/GettingStarted/LOGLCoordinateSystemsTab.h>
-#include <oscar_learnopengl/GettingStarted/LOGLHelloTriangleTab.h>
-#include <oscar_learnopengl/GettingStarted/LOGLTexturingTab.h>
-#include <oscar_learnopengl/Lighting/LOGLBasicLightingTab.h>
-#include <oscar_learnopengl/Lighting/LOGLLightingMapsTab.h>
-#include <oscar_learnopengl/Lighting/LOGLMultipleLightsTab.h>
-#include <oscar_learnopengl/PBR/LOGLPBRDiffuseIrradianceTab.h>
-#include <oscar_learnopengl/PBR/LOGLPBRLightingTab.h>
-#include <oscar_learnopengl/PBR/LOGLPBRLightingTexturedTab.h>
-#include <oscar_learnopengl/PBR/LOGLPBRSpecularIrradianceTab.h>
-#include <oscar_learnopengl/PBR/LOGLPBRSpecularIrradianceTexturedTab.h>
+#include <oscar_learnopengl/AdvancedLighting.h>
+#include <oscar_learnopengl/AdvancedOpenGL.h>
+#include <oscar_learnopengl/GettingStarted.h>
+#include <oscar_learnopengl/Lighting.h>
+#include <oscar_learnopengl/PBR.h>
 
 #include <oscar/Utils/Typelist.h>
 
