3f0245e5ff7ddf13fcf5cc5ab4d760e7579b4204
Adam Kewley <contact@adamkewley.com>
2024-02-27 14:58:21 +0100

    Add trivial tests for CachedModelWarper

---
+++ tests/TestOpenSimCreator/Documents/ModelWarper/TestCachedModelWarper.cpp
@@ -0,0 +1,27 @@
+#include <OpenSimCreator/Documents/ModelWarper/CachedModelWarper.h>
+
+#include <gtest/gtest.h>
+#include <OpenSim/Simulation/Model/Model.h>
+#include <OpenSimCreator/Documents/ModelWarper/Document.h>
+#include <OpenSimCreator/Utils/OpenSimHelpers.h>
+
+using namespace osc;
+using namespace osc::mow;
+
+TEST(CachedModelWarper, CanBeDefaultConstructed)
+{
+    ASSERT_NO_THROW({ CachedModelWarper{}; });
+}
+
+TEST(CachedModelWarper, CanWarpDefaultConstructedModelWarpingDocument)
+{
+    Document document;
+    CachedModelWarper warper;
+
+    auto rv = warper.warp(document);
+    ASSERT_NE(rv, nullptr);
+
+    // ... and it can be initialized etc. ready for UI usage
+    InitializeModel(*rv);
+    InitializeState(*rv);
+}
