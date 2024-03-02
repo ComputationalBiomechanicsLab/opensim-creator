3dc5d2f2fd410b27c91b08d21dd2343f2ef47ef3
Adam Kewley <contact@adamkewley.com>
2024-03-02 19:09:27 +0100

    Add tests for some mesh generators

--- tests/testoscar/Graphics/TestMeshGenerators.cpp
+++ tests/testoscar/Graphics/TestMeshGenerators.cpp
@@ -4,6 +4,7 @@
 #include <oscar/oscar.h>
 
 using namespace osc;
+using namespace osc::literals;
 
 TEST(GenerateTorusKnotMesh, DefaultCtorWorksFine)
 {
@@ -75,4 +76,135 @@ TEST(GenerateIcosahedronMesh, DefaultCtorWorksFine)
 TEST(GenerateIcosahedronMesh, WorksWithNonDefaultArgs)
 {
     ASSERT_NO_THROW(GenerateIcosahedronMesh(10.0f, 2));
+}
+
+TEST(GenerateDodecahedronMesh, DefaultCtorWorksFine)
+{
+    Mesh const m = GenerateDodecahedronMesh();
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateDodecahedronMesh, WorksWithNonDefaultArgs)
+{
+    Mesh const m = GenerateDodecahedronMesh(5.0f, 3);
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateOctahedronMesh, DefaultCtorWorksFine)
+{
+    Mesh const m = GenerateOctahedronMesh();
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateOctahedronMesh, WorksWithNonDefaultArgs)
+{
+    Mesh const m = GenerateOctahedronMesh(11.0f, 2);
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateTetrahedronMesh, DefaultCtorWorksFine)
+{
+    Mesh const m = GenerateTetrahedronMesh();
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateTetrahedronMesh, WorksWithNonDefaultArgs)
+{
+    Mesh const m = GenerateTetrahedronMesh(0.5f, 3);
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateLatheMesh, DefaultCtorWorksFine)
+{
+    Mesh const m = GenerateLatheMesh();
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateLatheMesh, WorksWithNonDefaultArgs)
+{
+    Mesh const m = GenerateLatheMesh(
+        {{{0.0f, 0.0f}, {1.0f, 1.0f}, {2.0f, 2.0f}, {3.0f, 3.0f}}},
+        32,
+        45_deg,
+        180_deg
+    );
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateCircleMesh2, DefaultCtorWorksFine)
+{
+    Mesh const m = GenerateCircleMesh2();
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateCircleMesh2, WorksWithNonDefaultArgs)
+{
+    Mesh const m = GenerateCircleMesh2(0.5f, 64, 90_deg, 80_deg);
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateRingMesh, DefaultCtorWorksFine)
+{
+    Mesh const m = GenerateRingMesh();
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateRingMesh, WorksWithNonDefaultArgs)
+{
+    Mesh const m = GenerateRingMesh(0.1f, 0.2f, 16, 3, 90_deg, 180_deg);
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateTorusMesh2, DefaultCtorWorksFine)
+{
+    Mesh const m = GenerateTorusMesh2();
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
+}
+
+TEST(GenerateTorusMesh2, WorksWithNonDefaultArgs)
+{
+    Mesh const m = GenerateTorusMesh2(0.2f, 0.3f, 4, 32, 180_deg);
+    ASSERT_TRUE(m.hasVerts());
+    ASSERT_TRUE(m.hasNormals());
+    ASSERT_TRUE(m.hasTexCoords());
+    ASSERT_FALSE(m.getIndices().empty());
 }
