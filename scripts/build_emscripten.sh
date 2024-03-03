732332adf17128f7855b0534eed73deec0856d40
Adam Kewley <contact@adamkewley.com>
2024-03-03 14:32:32 +0100

    Update third_party CMakeLists.txt to make each dependency optional and lift decision to build_emscripten.sh

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -19,7 +19,7 @@ set -xeuo pipefail
 # - this is a custom build for now because I can't build all dependencies
 #   with emsdk yet
 
-CXXFLAGS="-fexceptions --use-port=sdl2" CC=emcc CXX=em++ cmake -S third_party/ -B osc-deps-build -DOSCDEPS_EMSCRIPTEN=ON -DCMAKE_INSTALL_PREFIX=${PWD}/osc-deps-install -DCMAKE_INSTALL_LIBDIR=${PWD}/osc-deps-install/lib
+CXXFLAGS="-fexceptions --use-port=sdl2" CC=emcc CXX=em++ cmake -S third_party/ -B osc-deps-build -DOSCDEPS_GET_BENCHMARK=OFF -DOSCDEPS_GET_SDL=OFF -DOSCDEPS_GET_GLEW=OFF -DOSCDEPS_GET_NATIVEFILEDIALOG=OFF -DOSCDEPS_GET_OPENSIM=OFF -DOSCDEPS_EMSCRIPTEN=ON -DCMAKE_INSTALL_PREFIX=${PWD}/osc-deps-install -DCMAKE_INSTALL_LIBDIR=${PWD}/osc-deps-install/lib
 cmake --build osc-deps-build -j20 -v
 
 LDFLAGS="-fexceptions -sNO_DISABLE_EXCEPTION_CATCHING=1 -sLEGACY_GL_EMULATION=1 -sUSE_WEBGL2=1 -sUSE_SDL=2" CXXFLAGS="-fexceptions --use-port=sdl2" CC=emcc CXX=em++ cmake -S . -B osc-build -DOSC_EXECUTABLE_SUFFIX=".html" -DOSC_BUILD_OPENSIMCREATOR=OFF -DOSC_BUILD_BENCHMARKS=OFF -DOSC_BUILD_INSTALLABLE_PACKAGE=OFF -DOSC_DISCOVER_TESTS=OFF -DOSC_EMSCRIPTEN=ON -DCMAKE_PREFIX_PATH=${PWD}/osc-deps-install
