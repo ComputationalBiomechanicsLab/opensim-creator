bdde3381065a48bfadb5c668491f00fd4fdd0dd3
Adam Kewley <contact@adamkewley.com>
2024-03-05 16:30:54 +0100

    Hack in basic support for model.warpconfig.toml

--- src/OpenSimCreator/Documents/ModelWarper/ModelWarpConfiguration.cpp
+++ src/OpenSimCreator/Documents/ModelWarper/ModelWarpConfiguration.cpp
@@ -1,11 +1,25 @@
 #include "ModelWarpConfiguration.h"
 
+#include <oscar/Platform/Log.h>
+#include <toml++/toml.hpp>
+
 #include <filesystem>
 
 osc::mow::ModelWarpConfiguration::ModelWarpConfiguration(
-    std::filesystem::path const&,
+    std::filesystem::path const& osimFileLocation,
     OpenSim::Model const&)
 {
+    std::filesystem::path maybeWarpconfigLocation = osimFileLocation;
+    maybeWarpconfigLocation.replace_extension("warpconfig.toml");
+    if (std::filesystem::exists(maybeWarpconfigLocation)) {
+        auto parsed = toml::parse_file(maybeWarpconfigLocation.string());
+        if (auto globals = parsed.get_as<toml::table>("global_settings")) {
+            if (auto defaultWarps = globals->get_as<bool>("should_default_missing_frame_warps_to_identity")) {
+                m_ShouldDefaultMissingFrameWarpsToIdentity = defaultWarps->as_boolean();
+            }
+        }
+    }
+
     // TODO: go find the model warping configuration file on-disk "near" the osim
     //       file
 }
