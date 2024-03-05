39b6d9ba3628ab3bc2f9b507ac6dd6d9fb47aac9
Adam Kewley <contact@adamkewley.com>
2024-03-05 16:12:03 +0100

    Add IdentityFrameWarp and basic ModelWarpConfiguration override for using them

--- src/OpenSimCreator/Documents/ModelWarper/FrameWarpLookup.cpp
+++ src/OpenSimCreator/Documents/ModelWarper/FrameWarpLookup.cpp
@@ -1,7 +1,19 @@
 #include "FrameWarpLookup.h"
 
+#include <OpenSimCreator/Documents/ModelWarper/IdentityFrameWarp.h>
+#include <OpenSimCreator/Documents/ModelWarper/ModelWarpConfiguration.h>
+
+#include <OpenSim/Simulation/Model/Model.h>
+
 osc::mow::FrameWarpLookup::FrameWarpLookup(
     std::filesystem::path const&,
-    OpenSim::Model const&,
-    ModelWarpConfiguration const&)
-{}
+    OpenSim::Model const& model,
+    ModelWarpConfiguration const& config)
+{
+    for (OpenSim::Frame const& frame : model.getComponentList<OpenSim::Frame>()) {
+        // TODO: should actually look up the correct warping alg. etc.
+        if (config.getShouldDefaultMissingFrameWarpsToIdentity()) {
+            m_AbsPathToWarpLUT.try_emplace(frame.getAbsolutePathString(), std::make_unique<IdentityFrameWarp>());
+        }
+    }
+}
--- src/OpenSimCreator/Documents/ModelWarper/ModelWarpConfiguration.h
+++ src/OpenSimCreator/Documents/ModelWarper/ModelWarpConfiguration.h
@@ -19,7 +19,11 @@ namespace osc::mow
         float getWarpBlendingFactor() const { return m_WarpBlendingFactor; }
         void setWarpBlendingFactor(float v) { m_WarpBlendingFactor = saturate(v); }
 
+        bool getShouldDefaultMissingFrameWarpsToIdentity() const { return m_ShouldDefaultMissingFrameWarpsToIdentity; }
+        void setShouldDefaultMissingFrameWarpsToIdentity(bool v) { m_ShouldDefaultMissingFrameWarpsToIdentity = v; }
+
     private:
         float m_WarpBlendingFactor = 1.0f;
+        bool m_ShouldDefaultMissingFrameWarpsToIdentity = false;
     };
 }
