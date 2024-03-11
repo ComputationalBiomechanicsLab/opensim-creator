705e7b46633695410df8428db857835907e860a4
Adam Kewley <contact@adamkewley.com>
2024-03-11 16:41:04 +0100

    Add is_in_front_of plane-AABB testing function

--- src/oscar/Maths/PlaneFunctions.h
+++ src/oscar/Maths/PlaneFunctions.h
@@ -1,13 +1,27 @@
 #pragma once
 
+#include <oscar/Maths/AABB.h>
+#include <oscar/Maths/AABBFunctions.h>
+#include <oscar/Maths/CommonFunctions.h>
 #include <oscar/Maths/GeometricFunctions.h>
 #include <oscar/Maths/Plane.h>
 #include <oscar/Maths/Vec3.h>
 
 namespace osc
 {
+    // returns the signed distance between the (normal-oriented) surface of `plane` and `v`
     constexpr float signed_distance_between(Plane const& plane, Vec3 const& v)
     {
         return dot(v, plane.normal) - dot(plane.origin, plane.normal);
+    }
+
+    // tests if `aabb` is entirely in front of `plane`
+    constexpr bool is_in_front_of(Plane const& plane, AABB const& aabb)
+    {
+        // originally found in: https://learnopengl.com/Guest-Articles/2021/Scene/Frustum-Culling
+        // which was based on : https://gdbooks.gitbooks.io/3dcollisions/content/Chapter2/static_aabb_plane.html
+
+        float const r = dot(dimensions(aabb), abs(plane.normal));
+        return -r <= signed_distance_between(plane, centroid(aabb));
     }
 }
