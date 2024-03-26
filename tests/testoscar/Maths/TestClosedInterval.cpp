aa742112cbe40dd24f33ff2b6009b63a93437c67
Adam Kewley <contact@adamkewley.com>
2024-03-26 16:12:55 +0100

    Annotate ClosedInterval in tests with [[maybe_unused]]

--- tests/testoscar/Maths/TestClosedInterval.cpp
+++ tests/testoscar/Maths/TestClosedInterval.cpp
@@ -8,16 +8,16 @@ using namespace osc;
 
 TEST(ClosedInterval, CanConstructForInts)
 {
-    ClosedInterval<int>{0, 1}; // shouldn't throw etc
+    [[maybe_unused]] ClosedInterval<int> r{0, 1}; // shouldn't throw etc
 }
 
 TEST(ClosedInterval, ReversingOrderIsAllowed)
 {
-    ClosedInterval<int>{1, 0};
+    [[maybe_unused]] ClosedInterval<int> r{1, 0};
 }
 
 TEST(ClosedInterval, TimestampsAreAllowed)
 {
     using TP = std::chrono::system_clock::time_point;
-    ClosedInterval<TP>{TP{}, TP{} + std::chrono::seconds{1}};
+    [[maybe_unused]] ClosedInterval<TP> r{TP{}, TP{} + std::chrono::seconds{1}};
 }
