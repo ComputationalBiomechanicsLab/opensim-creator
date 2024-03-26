ae8e59598467e20cdc329a64491b79b79653a04e
Adam Kewley <contact@adamkewley.com>
2024-03-26 15:14:31 +0100

    Fix aggregate initialization in TestClosedInterval

--- tests/testoscar/Maths/TestClosedInterval.cpp
+++ tests/testoscar/Maths/TestClosedInterval.cpp
@@ -8,16 +8,16 @@ using namespace osc;
 
 TEST(ClosedInterval, CanConstructForInts)
 {
-    ASSERT_NO_THROW({ ClosedInterval<int>(0, 1); });
+    ClosedInterval<int>{0, 1}; // shouldn't throw etc
 }
 
 TEST(ClosedInterval, ReversingOrderIsAllowed)
 {
-    ASSERT_NO_THROW({ ClosedInterval<int>(1, 0); });
+    ClosedInterval<int>{1, 0};
 }
 
 TEST(ClosedInterval, TimestampsAreAllowed)
 {
     using TP = std::chrono::system_clock::time_point;
-    ASSERT_NO_THROW({ ClosedInterval<TP>(TP{}, TP{} + std::chrono::seconds{1}); });
+    ClosedInterval<TP>{TP{}, TP{} + std::chrono::seconds{1}};
 }
