459b9bd4b077cccdc49a333c6c7ddddf5c795d44
Adam Kewley <contact@adamkewley.com>
2024-06-13 16:01:44 +0200

    Get something rendering for hellotriangle

--- apps/hellotriangle/hellotriangle.cpp
+++ apps/hellotriangle/hellotriangle.cpp
@@ -6,15 +6,29 @@ namespace
 {
     class HelloTriangleScreen final : public IScreen {
     public:
+        HelloTriangleScreen()
+        {
+            const Vec3 viewer_pos = {3.0f, 0.0f, 0.0f};
+            camera_.set_position(viewer_pos);
+            camera_.set_direction({-1.0f, 0.0f, 0.0f});
+            const Color color = Color::red();
+            material_.set_ambient_color(0.2f * color);
+            material_.set_diffuse_color(0.5f * color);
+            material_.set_specular_color(0.5f * color);
+            material_.set_viewer_position(viewer_pos);
+        }
+    private:
          void impl_on_draw() override
          {
-            Camera camera;
-            graphics::draw(mesh_, identity<Transform>(), material_, camera);
-            camera.render_to_screen();
+             const auto secs = App::get().frame_delta_since_startup().count();
+             const auto transform = identity<Transform>().with_rotation(angle_axis(Radians{secs}, Vec3{0.0f, 1.0f, 0.0f}));
+             graphics::draw(mesh_, transform, material_, camera_);
+             camera_.render_to_screen();
          }
-    private:
-        Mesh mesh_ = TorusKnotGeometry{};
-        Material material_ = MeshBasicMaterial{};
+
+        TorusKnotGeometry mesh_;
+        MeshPhongMaterial material_;
+        Camera camera_;
     };
 }
 
