44bb680d52deef314eeac11bb61e25655787ab07
Adam Kewley <contact@adamkewley.com>
2024-07-01 15:41:10 +0200

    Add trivial test cases for the new osc::mow::ModelWarperConfiguration

--- src/OpenSimCreator/Documents/ModelWarper/ModelWarperConfiguration.h
+++ src/OpenSimCreator/Documents/ModelWarper/ModelWarperConfiguration.h
@@ -9,6 +9,7 @@ namespace osc::mow
     // abstract interface to a component that is capable of warping `n` other
     // components (`StrategyTargets`) during a model warp
     class ComponentWarpingStrategy : public OpenSim::Component {
+        OpenSim_DECLARE_ABSTRACT_OBJECT(ComponentWarpingStrategy, OpenSim::Component);
     protected:
         ComponentWarpingStrategy() = default;
         ComponentWarpingStrategy(const ComponentWarpingStrategy&) = default;
@@ -24,12 +25,15 @@ namespace osc::mow
 
     // abstract interface to a component that is capable of warping `n`
     // `OpenSim::PhysicalOffsetFrame`s during a model warp
-    class OffsetFrameWarpingStrategy : public ComponentWarpingStrategy {};
+    class OffsetFrameWarpingStrategy : public ComponentWarpingStrategy {
+        OpenSim_DECLARE_ABSTRACT_OBJECT(OffsetFrameWarpingStrategy, ComponentWarpingStrategy);
+    };
 
     // concrete implementation of an `OffsetFrameWarpingStrategy` in which
     // only the `translation` property of the offset frame is warped but the
     // rotation is left as-is
     class ThinPlateSplineOnlyTranslationOffsetFrameWarpingStrategy final : public OffsetFrameWarpingStrategy {
+        OpenSim_DECLARE_CONCRETE_OBJECT(ThinPlateSplineOnlyTranslationOffsetFrameWarpingStrategy, OffsetFrameWarpingStrategy);
     public:
         // PointSources
     private:
@@ -39,25 +43,39 @@ namespace osc::mow
     // implementation should produce a halting error rather than continuing with
     // the model warp
     class ProduceErrorOffsetFrameWarpingStrategy final : public OffsetFrameWarpingStrategy {
+        OpenSim_DECLARE_CONCRETE_OBJECT(ProduceErrorOffsetFrameWarpingStrategy, OffsetFrameWarpingStrategy);
     };
 
-    // concrete implementation of an `OffsetFrameWarpingStrategy` in which the
-    // implementation should use a least-squares fit of the correspondences between
-    // source/destination landmarks (`PointsToFit`) to compute the resulting offset
-    // frame's `translation` and `rotation`
-    class LeastSquaresOffsetFrameWarpingStrategy final : public OffsetFrameWarpingStrategy {
-    public:
-        // PointsToFit
+    // concrete implementation of an `OffsetFrameWarpingStrategy` in which the implementation
+    // simply copies the `translation` and `rotation` of the source `OpenSim::PhysicalOffsetFrame` to
+    // the destination model with no modifications
+    class IdentityOffsetFrameWarpingStrategy final : public OffsetFrameWarpingStrategy {
+        OpenSim_DECLARE_CONCRETE_OBJECT(IdentityOffsetFrameWarpingStrategy, OffsetFrameWarpingStrategy);
     };
 
     // abstract interface to a component that is capable of warping `n`
     // `OpenSim::Station`s during a model warp
-    class StationWarpingStrategy : public OpenSim::Component {};
+    class StationWarpingStrategy : public ComponentWarpingStrategy {
+        OpenSim_DECLARE_ABSTRACT_OBJECT(StationWarpingStrategy, ComponentWarpingStrategy);
+    };
 
     // concrete implementation of a `StationWarpingStrategy` that uses the Thin-Plate
     // Spline (TPS) algorithm to fit correspondences between mesh landmarks (`MeshSources`)
     class ThinPlateSplineStationWarpingStrategy final : public StationWarpingStrategy {
+        OpenSim_DECLARE_CONCRETE_OBJECT(ThinPlateSplineStationWarpingStrategy, StationWarpingStrategy);
         // MeshSources
+    };
+
+    // concrete implementation of a `StationWarpingStrategy` in which the implementation should
+    // produce a halting error rather than continuing with the model warp
+    class ProduceErrorStationWarpingStrategy final : public StationWarpingStrategy {
+        OpenSim_DECLARE_CONCRETE_OBJECT(ProduceErrorStationWarpingStrategy, StationWarpingStrategy);
+    };
+
+    // concrete implementation of a `StationWarpingStrategy` in which the implementation should
+    // just copy the station's postion (+parent) without any modification
+    class IdentityStationWarpingStrategy final : public StationWarpingStrategy {
+        OpenSim_DECLARE_CONCRETE_OBJECT(IdentityStationWarpingStrategy, StationWarpingStrategy);
     };
 
     // TODO:
--- tests/TestOpenSimCreator/Documents/ModelWarper/TestModelWarperConfiguration.cpp
+++ tests/TestOpenSimCreator/Documents/ModelWarper/TestModelWarperConfiguration.cpp
@@ -23,16 +23,67 @@ TEST(ModelWarperConfiguration, CanDefaultConstruct)
 	[[maybe_unused]] ModelWarperConfiguration configuration;
 }
 
-TEST(ModelWarperConfiguration, CanSaveDefaultConstructedToXMLFile)
+TEST(ModelWarperConfiguration, CanSaveAndLoadDefaultConstructedToAndFromXMLFile)
 {
-	TemporaryFile temporary_file;
-	temporary_file.close();  // so that `OpenSim::Object::print`'s implementation can open+write to it
+	TemporaryFile temporaryFile;
+	temporaryFile.close();  // so that `OpenSim::Object::print`'s implementation can open+write to it
 
 	ModelWarperConfiguration configuration;
-	configuration.print(temporary_file.absolute_path().string());
+	configuration.print(temporaryFile.absolute_path().string());
+
+	ModelWarperConfiguration loadedConfiguration{temporaryFile.absolute_path().string()};
+	loadedConfiguration.finalizeFromProperties();
+	loadedConfiguration.finalizeConnections(loadedConfiguration);
 }
 
 TEST(ModelWarperConfiguration, LoadingNonExistentFileThrows)
 {
 	ASSERT_ANY_THROW({ [[maybe_unused]] ModelWarperConfiguration configuration{GetFixturePath("doesnt_exist")}; });
+}
+
+TEST(ModelWarperConfiguration, CanLoadEmptySequence)
+{
+	ModelWarperConfiguration configuration{GetFixturePath("Document/ModelWarper/ModelWarperConfiguration/empty_sequence.xml")};
+	configuration.finalizeFromProperties();
+	configuration.finalizeConnections(configuration);
+}
+
+TEST(ModelWarperConfiguration, CanLoadTrivialSingleOffsetFrameWarpingStrategy)
+{
+	OpenSim::Object::registerType(ProduceErrorOffsetFrameWarpingStrategy{});
+
+	ModelWarperConfiguration configuration{GetFixturePath("Document/ModelWarper/ModelWarperConfiguration/single_offsetframe_warper.xml")};
+	configuration.finalizeFromProperties();
+	configuration.finalizeConnections(configuration);
+
+	auto range = configuration.getComponentList<ProduceErrorOffsetFrameWarpingStrategy>();
+	const auto numEls = std::distance(range.begin(), range.end());
+	ASSERT_EQ(numEls, 1);
+}
+
+TEST(ModelWarperConfiguration, CanContainAMixtureOfOffsetFrameWarpingStrategies)
+{
+	OpenSim::Object::registerType(ProduceErrorOffsetFrameWarpingStrategy{});
+	OpenSim::Object::registerType(ThinPlateSplineOnlyTranslationOffsetFrameWarpingStrategy{});
+
+	ModelWarperConfiguration configuration{GetFixturePath("Document/ModelWarper/ModelWarperConfiguration/mixed_offsetframe_warpers.xml")};
+	configuration.finalizeFromProperties();
+	configuration.finalizeConnections(configuration);
+
+	auto range = configuration.getComponentList<OffsetFrameWarpingStrategy>();
+	const auto numEls = std::distance(range.begin(), range.end());
+	ASSERT_EQ(numEls, 2);
+}
+
+TEST(ModelWarperConfiguration, CanLoadTrivialSingleStationWarpingStrategy)
+{
+	OpenSim::Object::registerType(ProduceErrorStationWarpingStrategy{});
+
+	ModelWarperConfiguration configuration{GetFixturePath("Document/ModelWarper/ModelWarperConfiguration/single_station_warper.xml")};
+	configuration.finalizeFromProperties();
+	configuration.finalizeConnections(configuration);
+
+	auto range = configuration.getComponentList<ProduceErrorStationWarpingStrategy>();
+	const auto numEls = std::distance(range.begin(), range.end());
+	ASSERT_EQ(numEls, 1);
 }
