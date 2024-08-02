54ff99d5402c159897eeed6b3461b7d5700bfece
Adam Kewley <contact@adamkewley.com>
2024-08-02 11:35:05 +0200

    Add missing em_js.h header

--- src/oscar/UI/imgui_impl_sdl2.cpp
+++ src/oscar/UI/imgui_impl_sdl2.cpp
@@ -100,6 +100,9 @@
 #if defined(__APPLE__)
 #include <TargetConditionals.h>
 #endif
+#ifdef __EMSCRIPTEN__
+#include <emscripten/em_js.h>
+#endif
 
 #if SDL_VERSION_ATLEAST(2,0,4) && !defined(__EMSCRIPTEN__) && !defined(__ANDROID__) && !(defined(__APPLE__) && TARGET_OS_IOS) && !defined(__amigaos4__)
 #define SDL_HAS_CAPTURE_AND_GLOBAL_MOUSE    1
