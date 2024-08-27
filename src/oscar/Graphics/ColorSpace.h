997712065b5d2c60e72302e5175f20fb32918916
Adam Kewley <contact@adamkewley.com>
2024-08-27 11:42:51 +0200

    Improve ColorSpace documentation

--- src/oscar/Graphics/ColorSpace.h
+++ src/oscar/Graphics/ColorSpace.h
@@ -2,17 +2,21 @@
 
 namespace osc
 {
-    // used to tell the renderer the color space of provided pixel data
+    // Used in various parts of the codebase (e.g. image loaders) to indicate the
+    // color space of the provided pixel data.
     //
-    // this is necessary because OSC assumes that all color data within shaders
-    // is within a linear color space, but the color space of external data (e.g.
-    // loaded from images) will typically be in sRGB color space and require on-GPU
-    // conversion
+    // This is necessary because some functions are only capable of knowing that they
+    // are loading pixel data (e.g. as RGB bytes), but they aren't capable of knowing
+    // the color space encoding of that data (e.g. when loading a normal map from a
+    // PNG). It's important metadata, because shaders always work in a linear color space,
+    // and other calling code may only want to deal with sRGB colors (e.g. even if
+    // the colors were originally loaded as linear-space floats)
     enum class ColorSpace {
-        // hint: typical image files (e.g. from photo editors, online, etc.) are defined in sRGB
+
+        // Indicates an sRGB colorspace. This is typical for (e.g.) image files, albedo textures
         sRGB,
 
-        // hint: data image files (e.g. bump maps, normal maps) are usually defined in a linear color space
+        // Indicates a linear colorspace. This is typical for (e.g.) bump maps, normal maps
         Linear,
 
         NUM_OPTIONS,
