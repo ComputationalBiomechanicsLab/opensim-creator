7d82a256883462d86a6167e5264266e0b54bae39
Adam Kewley <contact@adamkewley.com>
2024-09-04 09:49:52 +0200

    Add algorithmic drawing example to bookofshaders demos

--- src/oscar_bookofshaders/BookOfShadersTab.cpp
+++ src/oscar_bookofshaders/BookOfShadersTab.cpp
@@ -93,6 +93,40 @@ void main() {
 
 )";
     };
+
+    class AlgorithmicDrawing_LinePlot final : public BookOfShadersShader {
+    public:
+        AlgorithmicDrawing_LinePlot() : BookOfShadersShader{c_fragment_source} {}
+    private:
+        static constexpr CStringView c_fragment_source = R"(
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+// Plot a line on Y using a value between 0.0-1.0
+float plot(vec2 st) {
+    return smoothstep(0.02, 0.0, abs(st.y - st.x));
+}
+
+void main() {
+	vec2 st = gl_FragCoord.xy/u_resolution;
+
+    float y = st.x;
+
+    vec3 color = vec3(y);
+
+    // Plot a line
+    float pct = plot(st);
+    color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
+
+	gl_FragColor = vec4(color,1.0);
+}
+)";
+    };
 }
 
 class osc::BookOfShadersTab::Impl final : public StandardTabImpl {
@@ -140,6 +174,7 @@ private:
         {"hello_world", HelloWorldMaterial{}},
         {"uniforms_time_colored", Uniforms_TimeColored{}},
         {"uniforms_gl_FragCoord", Uniforms_gl_FragCoord{}},
+        {"algorithmic_drawing", AlgorithmicDrawing_LinePlot{}},
     };
     std::string current_material_ = materials_.begin()->first;
     PlaneGeometry quad_;
