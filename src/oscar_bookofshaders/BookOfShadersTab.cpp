75614148516a5be399b4bd5ebf78f7dcba8c65be
Adam Kewley <contact@adamkewley.com>
2024-09-05 15:11:56 +0200

    Minor formatting change

--- src/oscar_bookofshaders/BookOfShadersTab.cpp
+++ src/oscar_bookofshaders/BookOfShadersTab.cpp
@@ -97,7 +97,7 @@ float plot(vec2 st) {
 }
 
 void main() {
-	vec2 st = gl_FragCoord.xy/u_resolution;
+    vec2 st = gl_FragCoord.xy/u_resolution;
 
     float y = st.x;
 
@@ -107,7 +107,7 @@ void main() {
     float pct = plot(st);
     color = mix(color, vec3(0.0, 1.0, 0.0), pct);
 
-	gl_FragColor = vec4(color,1.0);
+    gl_FragColor = vec4(color,1.0);
 }
 )";
     };
