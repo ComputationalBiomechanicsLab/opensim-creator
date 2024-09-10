eb19f4f1641b31d4ff67263c20ff6a83523f8dfc
Adam Kewley <contact@adamkewley.com>
2024-09-10 12:54:49 +0200

    Move PreviewExperimentalDataTab into its own directory

--- src/OpenSimCreator/UI/OpenSimCreatorTabs.h
+++ src/OpenSimCreator/UI/OpenSimCreatorTabs.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <OpenSimCreator/UI/Experimental/PreviewExperimentalDataTab.h>
 #include <OpenSimCreator/UI/FrameDefinition/FrameDefinitionTab.h>
 #include <OpenSimCreator/UI/MeshWarper/MeshWarpingTab.h>
 #include <OpenSimCreator/UI/ModelWarper/ModelWarperTab.h>
+#include <OpenSimCreator/UI/PreviewExperimentalData/PreviewExperimentalDataTab.h>
 
 #include <oscar/Utils/Typelist.h>
 
