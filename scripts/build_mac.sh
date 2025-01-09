0edd602d9cf1954446cfa46f573c5347a8e9bf94
Adam Kewley <contact@adamkewley.com>
2025-01-09 13:54:59 +0100

    Implement necessary MacOS modifications for arm64 build (offline)

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -4,6 +4,8 @@
 # mac platform
 #
 #     usage (must be ran in repository root): `bash build_mac.sh`
+#
+# this assumes the system already has the necessary dependencies installed
 
 
 # error out of this script if it fails for any reason
@@ -21,9 +23,6 @@ OSC_DEPS_BUILD_TYPE=${OSC_DEPS_BUILD_TYPE:-`echo ${OSC_BASE_BUILD_TYPE}`}
 # build type for OSC
 OSC_BUILD_TYPE=${OSC_BUILD_TYPE-`echo ${OSC_BASE_BUILD_TYPE}`}
 
-# extra compiler flags for the C++ compiler
-OSC_CXX_FLAGS=${OSC_CXX_FLAGS:-`echo -fno-omit-frame-pointer`}
-
 # maximum number of build jobs to run concurrently
 #
 # defaulted to 1, rather than `sysctl -n hw.physicalcpu`, because OpenSim
@@ -31,8 +30,11 @@ OSC_CXX_FLAGS=${OSC_CXX_FLAGS:-`echo -fno-omit-frame-pointer`}
 # concurrently, #659
 OSC_BUILD_CONCURRENCY=${OSC_BUILD_CONCURRENCY:-1}
 
-# which build system to use (e.g. Ninja, Makefile: see https://cmake.org/cmake/help/latest/manual/cmake-generators.7.html)
-OSC_GENERATOR=${OSC_GENERATOR:-`echo Unix Makefiles`}
+# extra flags to pass into each configuration call to cmake
+#
+# this can be useful for (e.g.) specifying -DCMAKE_OSX_SYSROOT
+# and -G (generator) flags
+OSC_CMAKE_CONFIG_EXTRA=${OSC_CMAKE_CONFIG_EXTRA:-""}
 
 # which OSC build target to build
 #
@@ -40,77 +42,40 @@ OSC_GENERATOR=${OSC_GENERATOR:-`echo Unix Makefiles`}
 #     package  package everything into a .dmg installer
 OSC_BUILD_TARGET=${OSC_BUILD_TARGET:-package}
 
-# set this if you want to skip installing system-level deps
-#
-#     OSC_SKIP_BREW
-
-# set this if you want to build the documentation
-#
-#     OSC_BUILD_DOCS
-
 set +x
 echo "----- starting build -----"
 echo ""
 echo "----- printing build parameters -----"
 echo ""
-echo "    OSC_BASE_BUILD_TYPE   = ${OSC_BASE_BUILD_TYPE}"
-echo "    OSC_DEPS_BUILD_TYPE   = ${OSC_DEPS_BUILD_TYPE}"
-echo "    OSC_BUILD_TYPE        = ${OSC_BUILD_TYPE}"
-echo "    OSC_CXX_FLAGS         = ${OSC_CXX_FLAGS}"
-echo "    OSC_BUILD_CONCURRENCY = ${OSC_BUILD_CONCURRENCY}"
-echo "    OSC_GENERATOR         = ${OSC_GENERATOR}"
-echo "    OSC_BUILD_TARGET      = ${OSC_BUILD_TARGET}"
-echo "    OSC_SKIP_BREW         = ${OSC_SKIP_BREW:-OFF}"
-echo "    OSC_BUILD_DOCS        = ${OSC_BUILD_DOCS:-OFF}"
+echo "    OSC_BASE_BUILD_TYPE    = ${OSC_BASE_BUILD_TYPE}"
+echo "    OSC_DEPS_BUILD_TYPE    = ${OSC_DEPS_BUILD_TYPE}"
+echo "    OSC_BUILD_TYPE         = ${OSC_BUILD_TYPE}"
+echo "    OSC_BUILD_CONCURRENCY  = ${OSC_BUILD_CONCURRENCY}"
+echo "    OSC_CMAKE_CONFIG_EXTRA = ${OSC_CMAKE_CONFIG_EXTRA}"
+echo "    OSC_BUILD_TARGET       = ${OSC_BUILD_TARGET}"
 echo ""
 set -x
 
-
-echo "----- ensuring all submodules are up-to-date -----"
-git submodule update --init --recursive
-
-
-if [[ -z ${OSC_SKIP_BREW:+x} ]]; then
-    echo "----- getting system-level dependencies -----"
-
-    # `wget`
-    #
-    # seems to be a transitive dependency of OpenSim (Metis),
-    # which uses wget to get other deps
-    # brew install wget  # unnecessary: it's usually already installed
-
-    # osc: docs dependencies
-    # [[ ! -z ${OSC_BUILD_DOCS:+z} ]] && brew install python3  # unnecessary: it's usually already installed
-    [[ ! -z ${OSC_BUILD_DOCS:+z} ]] && pip3 install --user wheel
-    [[ ! -z ${OSC_BUILD_DOCS:+z} ]] && pip3 install --user -r docs/requirements.txt
-
-    # ensure sphinx-build is available on this terminal's PATH
-    [[ ! -z ${OSC_BUILD_DOCS:+z} ]] && ls "$(python3 -m site --user-base)/bin"
-    [[ ! -z ${OSC_BUILD_DOCS:+z} ]] && export PATH=${PATH}:"$(python3 -m site --user-base)/bin"
-
-    echo "----- finished getting system-level dependencies -----"
-else
-    echo "----- skipping getting system-level dependencies (OSC_SKIP_BREW) is
-    set -----"
-fi
 
 echo "----- PATH -----"
 echo "${PATH}"
 echo "----- /PATH -----"
 
-echo "----- printing system (post-dependency install) info -----"
+echo "----- printing system info -----"
 cc --version
 c++ --version
 cmake --version
 make --version
-[[ ! -z ${OSC_BUILD_DOCS:+z} ]] && sphinx-build --version  # required when building docs
+python3 --version
 
 echo "----- building OSC's dependencies -----"
 cmake \
     -S third_party \
     -B "osc-deps-build" \
     -DCMAKE_BUILD_TYPE=${OSC_DEPS_BUILD_TYPE} \
-    -DCMAKE_INSTALL_PREFIX="osc-deps-install"
+    -DCMAKE_INSTALL_PREFIX="osc-deps-install" \
+    ${OSC_CMAKE_CONFIG_EXTRA}
+
 cmake \
     --build "osc-deps-build" \
     -j${OSC_BUILD_CONCURRENCY}
@@ -123,7 +88,7 @@ cmake \
     -B "osc-build" \
     -DCMAKE_BUILD_TYPE=${OSC_BUILD_TYPE} \
     -DCMAKE_PREFIX_PATH="${PWD}/osc-deps-install" \
-    ${OSC_BUILD_DOCS:+-DOSC_BUILD_DOCS=ON}
+    ${OSC_CMAKE_CONFIG_EXTRA}
 
 # build all
 cmake \
