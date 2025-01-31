c6cf8874a30ed412f274f428dfb34c09f5165b41
Adam Kewley <contact@adamkewley.com>
2025-01-31 15:44:38 +0100

    Add bundle_sources.sh

---
+++ scripts/bundle_sources.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+git ls-files --recurse-submodules | tar caf ../opensimcreator-${1}-src.tar.xz --xform s:^:opensimcreator_${1}/: --verbatim-files-from -T-
+
