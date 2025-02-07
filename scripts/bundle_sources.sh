f80cec1e9b8dfe70d5efeecda6460cd827260237
Adam Kewley <contact@adamkewley.com>
2025-02-07 14:48:40 +0100

    Fix minor typo in bundle_sources.sh

--- scripts/bundle_sources.sh
+++ scripts/bundle_sources.sh
@@ -2,5 +2,5 @@
 
 set -xeuo pipefail
 
-git ls-files --recurse-submodules | tar caf ../opensimcreator-${1}-src.tar.xz --xform s:^:opensimcreator_${1}/: --verbatim-files-from -T-
+git ls-files --recurse-submodules | tar caf ../opensimcreator-${1}-src.tar.xz --xform s:^:opensimcreator-${1}/: --verbatim-files-from -T-
 
