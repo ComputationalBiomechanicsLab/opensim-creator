cf7182b5a55af27a9d12b201e11a7bfc3690b417
Adam Kewley <contact@adamkewley.com>
2025-02-07 12:07:45 +0100

    Add create-video-thumbnails.sh

---
+++ scripts/create-video-thumbnails.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This script is used in `files.opensimcreator.com` to create
+# thumbnail images for each video file, so that the website
+# doesn't have to load the video file in the gallery page
+#
+# On Windows, you can use MSYS2 with `ffmpeg` installed to
+# do this.
+#
+# This script's usually called with `files.opensimcreator.com`
+# as the working directory.
+
+for video_file in $(ls videos/*.{webm,mp4}); do
+    video_filename=$(basename -- "${video_file}")
+    thumbnail_filename="videos/${video_filename%.*}_thumbnail.jpg"
+    if [[ ! -e "${thumbnail_filename}" ]]; then
+    	ffmpeg -i "${video_file}" -s 480x270 -q:v 2 -frames:v 1 -y "${thumbnail_filename}"
+    fi
+done
+
