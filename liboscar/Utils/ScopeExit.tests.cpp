484bdf81aec719f9e23393a0263baeecfcbac9dc
Adam Kewley <contact@adamkewley.com>
2025-02-19 11:43:29 +0100

    Format ScopeExit.tests.cpp

--- liboscar/Utils/ScopeExit.tests.cpp
+++ liboscar/Utils/ScopeExit.tests.cpp
@@ -6,19 +6,19 @@ using namespace osc;
 
 TEST(ScopeExit, calls_exit_function_on_destruction)
 {
-	bool called = false;
-	{
-		ScopeExit guard{[&called]{ called = true; }};
-	}
-	ASSERT_TRUE(called);
+    bool called = false;
+    {
+        ScopeExit guard{[&called]{ called = true; }};
+    }
+    ASSERT_TRUE(called);
 }
 
 TEST(ScopeExit, release_prevents_exit_function_from_being_called)
 {
-	bool called = false;
-	{
-		ScopeExit guard{[&called]{ called = true; }};
-		guard.release();
-	}
-	ASSERT_FALSE(called);
+    bool called = false;
+    {
+        ScopeExit guard{[&called]{ called = true; }};
+        guard.release();
+    }
+    ASSERT_FALSE(called);
 }
