afc51b188ff68d7117d639904ae911e7a3615394
Adam Kewley <contact@adamkewley.com>
2025-03-06 16:05:10 +0100

    Address JvB March 2025 comments

--- paper/build.sh
+++ paper/build.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm --volume /home/adam/opensim-creator/paper/paper:/data --user 1000:1000 --env JOURNAL=joss openjournals/inara
+sudo docker run --rm --volume ${PWD}/paper:/data --user 1000:1000 --env JOURNAL=joss openjournals/inara
