e5a7f37136c9276e566c0c140c510910af6bba1c
Adam Kewley <contact@adamkewley.com>
2025-03-14 12:54:15 +0100

    Update paper from AS's feedback

--- paper/build.sh
+++ paper/build.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm --volume ${PWD}/paper:/data --user 1000:1000 --env JOURNAL=joss openjournals/inara
+sudo docker run --rm --volume ${PWD}/paper:/data --user 1000:1000 --env JOURNAL=joss openjournals/inara -o pdf,tex paper.md
