a23217e482cf2290600f44c71378c3652c142b83
Adam Kewley <contact@adamkewley.com>
2025-05-13 12:16:00 +0200

    Update XCode version and ensure compiler targets 14.2

--- scripts/build_mac-amd64.sh
+++ scripts/build_mac-amd64.sh
@@ -3,6 +3,7 @@
 # Performs an end-to-end build of OpenSim Creator for
 # amd64 (i.e. intel/AMD x86_64) MacOS systems.
 
-export OSC_CMAKE_CONFIG_EXTRA="-Werror=dev -DCMAKE_OSX_DEPLOYMENT_TARGET=14.2 -DCMAKE_OSX_ARCHITECTURES=x86_64"
+export OSC_CMAKE_CONFIG_EXTRA="-DCMAKE_OSX_DEPLOYMENT_TARGET=14.2 -DCMAKE_OSX_ARCHITECTURES=x86_64"
 export OSC_BUILD_CONCURRENCY=$(sysctl -n hw.ncpu)
 ./scripts/build_mac.sh
+
