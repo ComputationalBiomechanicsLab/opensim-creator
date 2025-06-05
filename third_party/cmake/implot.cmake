8e93eae361a29a31e19b13e69244cb73bd003d37
Adam Kewley <contact@adamkewley.com>
2025-06-05 10:50:47 +0200

    Rewrite third_party/ cmake build to patch in custom CMakeLists.txt etc. files

---
+++ third_party/cmake/implot.cmake
@@ -0,0 +1,49 @@
+cmake_minimum_required(VERSION 3.10)
+project(implot)
+
+include(GNUInstallDirs)  # CMAKE_INSTALL_LIBDIR
+include(CMakePackageConfigHelpers)  # configure_package_config_file
+
+find_package(imgui REQUIRED)
+
+add_library(implot STATIC
+    implot.cpp
+    implot.h
+    implot_demo.cpp
+    implot_internal.h
+    implot_items.cpp
+)
+target_include_directories(implot
+    PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}
+    INTERFACE $<INSTALL_INTERFACE:include/implot>
+)
+target_compile_features(implot
+    PRIVATE cxx_std_17
+)
+target_link_libraries(implot PUBLIC imgui)
+
+install(
+    TARGETS implot
+    EXPORT implotTargets
+)
+install(
+    FILES
+        implot.h
+    DESTINATION
+        include/implot
+)
+install(
+    EXPORT implotTargets
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/implot
+)
+configure_package_config_file(
+    "${CMAKE_CURRENT_SOURCE_DIR}/implotConfig.cmake.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/implotConfig.cmake"
+    INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/implot
+)
+install(
+    FILES
+        "${CMAKE_CURRENT_BINARY_DIR}/implotConfig.cmake"
+    DESTINATION
+        ${CMAKE_INSTALL_LIBDIR}/cmake/implot
+)
