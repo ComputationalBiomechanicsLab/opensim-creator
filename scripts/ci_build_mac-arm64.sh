892b6f3f7c068defebd65a6aecb4491b31037d74
Adam Kewley <contact@adamkewley.com>
2025-09-24 09:22:26 +0200

    Remove redundant variable setters from ci_build_mac-ARCH.sh

--- scripts/ci_build_mac-arm64.sh
+++ scripts/ci_build_mac-arm64.sh
@@ -6,14 +6,4 @@
 
 set -xeuo pipefail
 
-# Hide window creation in CI, because CI runners typically do not have
-# a desktop environment.
-export OSC_INTERNAL_HIDE_WINDOW="1"
-
-# Ensure dependencies are re-checked/re-built if the CI script is ran on
-# a potentially stale/cached workspace directory.
-export OSCDEPS_BUILD_ALWAYS=${OSCDEPS_BUILD_ALWAYS:-ON}
-
-export OSC_OSX_ARCHITECTURES=arm64
-
-./scripts/ci_build_mac.sh
+OSC_OSX_ARCHITECTURES=arm64 ./scripts/ci_build_mac.sh
