3caa39b2ba4c531c6cde0cab91a840159cd36974
Adam Kewley <contact@adamkewley.com>
2025-10-06 15:17:36 +0200

    Add executable flag to scripts, make build analsis run in Debug mode

--- scripts/analyze_clang-build-times.sh
+++ scripts/analyze_clang-build-times.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf build-clangbuildanalyzer  # clean out existing build
-CC=clang CXX=clang++ CXXFLAGS=-ftime-trace cmake -G Ninja -S . -B build-clangbuildanalyzer -DCMAKE_PREFIX_PATH=${PWD}/third_party-install-RelWithDebInfo -DOSC_BUILD_OPENSIMCREATOR=OFF
+CC=clang CXX=clang++ CXXFLAGS=-ftime-trace cmake -G Ninja -S . -B build-clangbuildanalyzer -DCMAKE_PREFIX_PATH=${PWD}/third_party-install-Debug -DOSC_BUILD_OPENSIMCREATOR=OFF
 ClangBuildAnalyzer --start build-clangbuildanalyzer/
 cmake --build build-clangbuildanalyzer/
 ClangBuildAnalyzer --stop build-clangbuildanalyzer/ cba-capture
