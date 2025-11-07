6f302ee10711629e0513695dd2f992b24bd320d0
Adam Kewley <contact@adamkewley.com>
2025-11-07 09:50:50 +0100

    Clean up CMakeLists.txt etc. a little

--- opynsim/debugger/debugscript.py
+++ opynsim/debugger/debugscript.py
@@ -1,5 +1,5 @@
 import opynsim
 
 # put code here that you'd like to run via your IDE's debugger
-# and then run the `opynsim_debugger` target, which will bootstrap
-# everything.
+# and then run the `opynsim_debugger.exe` target, which will bootstrap
+# everything and then run this script.
