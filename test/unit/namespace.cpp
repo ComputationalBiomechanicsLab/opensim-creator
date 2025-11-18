8ce540dc51e7ecebdb194978368b0b229764d8ae
Adam Kewley <contact@adamkewley.com>
2025-11-18 13:53:08 +0100

    Squashed 'third_party/unordered_dense/' content from commit 3234af2c03
    git-subtree-dir: third_party/unordered_dense
    git-subtree-split: 3234af2c03549bc85656bfd3a86993bf1cd8aef1

---
+++ test/unit/namespace.cpp
@@ -0,0 +1,12 @@
+#include <ankerl/unordered_dense.h>
+#include <app/doctest.h>
+
+namespace versioned_namespace = ankerl::unordered_dense::v4_8_1;
+
+static_assert(std::is_same_v<versioned_namespace::map<int, int>, ankerl::unordered_dense::map<int, int>>);
+static_assert(std::is_same_v<versioned_namespace::hash<int>, ankerl::unordered_dense::hash<int>>);
+
+TEST_CASE("version_namespace") {
+    auto map = versioned_namespace::map<int, int>{};
+    REQUIRE(map.empty());
+}
