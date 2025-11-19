97d2d5db42b6f11cf0e953491d09dfd45904dcbc
Adam Kewley <contact@adamkewley.com>
2025-11-19 12:19:53 +0100

    Clean up manylinux build

--- scripts/e2e_setup-and-build-manylinux234.sh
+++ scripts/e2e_setup-and-build-manylinux234.sh
@@ -6,4 +6,26 @@
 #
 # manylinux_2_34: Debian 12+, Ubuntu 21.10+, Fedora 35+, CentOS/RHEL 9+
 
-sudo docker run --rm -v ${PWD}:/opynsim quay.io/pypa/manylinux_2_34_x86_64 bash -c 'yum install -y epel-release && yum install -y ninja-build && cd opynsim && PATH=/opt/python/cp312-cp312/bin:$PATH ./scripts/e2e_setup-and-build-unix.sh'
+sudo docker run --rm -v ${PWD}:/opynsim quay.io/pypa/manylinux_2_34_x86_64 bash -c "$(cat <<'EOF'
+set -xeuo pipefail
+
+export PATH=/opt/python/cp312-cp312/bin:$PATH
+export CMAKE_BUILD_PARALLEL_LEVEL=$(nproc)
+
+# go into mapped directory
+cd opynsim/
+
+# setup virtual environment
+./scripts/setup_venv.py
+
+# build bundled dependencies
+cd third_party/ && cmake --workflow --preset OPynSim_third-party_ManyLinux && cd -
+
+# source the project-level virtual environment
+source ./scripts/env_venv.sh
+
+# build the project
+cmake --workflow --preset OPynSim_ManyLinux
+EOF
+)"
+
