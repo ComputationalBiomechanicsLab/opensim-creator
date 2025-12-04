44b60e261df4ca8d764ac914ddde8aafe7a03f20
Adam Kewley <contact@adamkewley.com>
2025-12-04 11:34:18 +0100

    Add e2e_build_windows.bat, simplify GitHub Action

---
+++ scripts/e2e_build_windows.bat
@@ -0,0 +1,34 @@
+REM set default configuration if none provided
+IF "%~1"=="" (
+    set CONFIGS=Release
+) ELSE (
+    set CONFIGS=%*
+)
+
+REM Ensure this script uses the Visual Studio (C++) environment
+call "scripts/env_vs-x64.bat"
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to source the Visual Studio environment
+    exit /b %ERRORLEVEL%
+)
+
+REM Perform specified end-to-end builds
+FOR %%C IN (%CONFIGS%) DO (
+    REM build dependencies
+    echo Entering third_party directory for %%C
+    cd third_party
+    cmake --workflow --preset %%C
+    IF !ERRORLEVEL! NEQ 0 (
+       echo Failed to build dependencies for %%C
+       exit /b !ERRORLEVEL!
+    )
+    cd ..
+
+    REM build the project
+    echo Building main project for %%C
+    cmake --workflow --preset %%C
+    IF !ERRORLEVEL! NEQ 0 (
+       echo Failed to build main project for %%C
+       exit /b !ERRORLEVEL!
+    )
+)
