0eb5be12d8d1c869dc0f65a3438d05b3093f1b4e
Adam Kewley <contact@adamkewley.com>
2025-12-04 15:20:36 +0100

    Add gtest main stub for TestOpenSimCreator (can help debugging)

---
+++ libopensimcreator/testing/TestOpenSimCreator.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+
+int main(int argc, char** argv)
+{
+    // This little dance can be handy for modifying what `TestOpenSimCreator` does when
+    // ran from an IDE where editing the run configuration is annoying to do.
+    std::vector<std::string> extra_args = {
+        // "--gtest_filter=UndoableModelStatePair.CanLoadModelWithMuscleEquilibrationProblems",  // e.g.
+    };
+    std::vector<char*> forwarded_args;
+    forwarded_args.reserve(static_cast<size_t>(argc) + extra_args.size());
+    forwarded_args.insert(forwarded_args.end(), argv, argv + argc);
+    for (auto& extra_arg : extra_args) {
+        forwarded_args.push_back(extra_arg.data());
+    }
+    auto size = static_cast<int>(forwarded_args.size());
+
+    testing::InitGoogleTest(&size, forwarded_args.data());
+    return RUN_ALL_TESTS();
+}
