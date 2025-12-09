92769b164407bb2d31a89f4c7d4a4070819db93f
Adam Kewley <contact@adamkewley.com>
2025-12-09 14:54:20 +0100

    Add is_directory check to ResourceStream

--- liboscar/Platform/ResourceStream.cpp
+++ liboscar/Platform/ResourceStream.cpp
@@ -11,6 +11,15 @@ namespace
 {
     std::unique_ptr<std::istream> open_stream_or_throw(const std::filesystem::path& path)
     {
+        // This pre-check is necessary because MacOS actually allows opening
+        // a `std::ifstream` to a directory because it handles all paths as
+        // valid file descriptors.
+        if (std::filesystem::is_directory(path)) {
+            std::stringstream ss;
+            ss << path.string() << ": is a directory, not a file";
+            throw std::runtime_error{std::move(ss).str()};
+        }
+
         auto rv = std::make_unique<std::ifstream>(path, std::ios::binary | std::ios::in);
         if (not (*rv)) {
             std::stringstream ss;
