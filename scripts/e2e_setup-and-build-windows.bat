5a2160ae15165311f76fe39cd5c18207b6faf8b8
Adam Kewley <contact@adamkewley.com>
2025-12-12 13:54:02 +0100

    Add e2e_setup-and-build-windows.bat

---
+++ scripts/e2e_setup-and-build-windows.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM performs an end-to-end build of opynsim on Windows
+
+REM set default configuration if none provided
+IF "%~1"=="" (
+    set CONFIGS=Release
+) ELSE (
+    set CONFIGS=%*
+)
+
+REM Environment activation: ensure this script uses the Visual Studio (C++) and python virtual environment
+call "scripts/env_vs-x64.bat"
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to source the Visual Studio environment
+    exit /b %ERRORLEVEL%
+)
+
+REM Perform specified end-to-end builds
+FOR %%C IN (%CONFIGS%) DO (
+    REM build dependencies
+    echo Entering third_party directory for %%C
+    cd third_party
+    cmake --workflow --preset %%C
+    IF !ERRORLEVEL! NEQ 0 (
+       echo Failed to build opynsim's dependencies
+       exit /b !ERRORLEVEL!
+    )
+    cd ..
+
+    REM build the project
+    echo Building main project for %%C
+    cmake --workflow --preset %%C
+    IF !ERRORLEVEL! NEQ 0 (
+       echo Failed to build opynsim for %%C
+       exit /b !ERRORLEVEL!
+    )
+)
+
+endlocal
