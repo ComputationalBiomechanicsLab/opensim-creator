afb2b291b36b4518977cdbcd05f4d36aabe37aa4
Adam Kewley <contact@adamkewley.com>
2025-12-18 16:01:51 +0100

    Disambiguate LandmarkPair default constructor when T==float vs. T==double

--- libopynsim/Utils/LandmarkPair3D.h
+++ libopynsim/Utils/LandmarkPair3D.h
@@ -14,8 +14,8 @@ namespace opyn
     struct LandmarkPair3D {
         friend bool operator==(const LandmarkPair3D&, const LandmarkPair3D&) = default;
 
-        SimTK::Vec<3, T> source{0.0};
-        SimTK::Vec<3, T> destination{0.0};
+        SimTK::Vec<3, T> source{T{0.0}};
+        SimTK::Vec<3, T> destination{T{0.0}};
     };
 
     std::ostream& operator<<(std::ostream&, const LandmarkPair3D<float>&);
