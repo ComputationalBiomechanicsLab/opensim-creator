5a8ba542592413cd1c3ed5abd7eb7b9ae546f04a
Adam Kewley <contact@adamkewley.com>
2026-01-06 14:10:03 +0100

    Clearly separate build/installation from packaging for MacOS.

--- packaging/MacOS/Packaging.cmake
+++ packaging/MacOS/Packaging.cmake
@@ -1,25 +1,17 @@
 # Apple MacOS packaging script
 #
-# Creates a DMG (archive) installer that packages the whole application into
+# Creates a DMG (archive) that packages the whole application into
 # a single directory tree that can be dragged to `/Applications`
 
-option(OSC_CODESIGN_ENABLED     "Enable codesigning the resulting app bundle and DMG file"    OFF)
+option(OSC_CODESIGN_ENABLED     "Enable codesigning the resulting application bundle and DMG file"    OFF)
 option(OSC_NOTARIZATION_ENABLED "Enable notarizing (xcrun notarytool) the resulting DMG file" OFF)
 
-# Package the software into a DMG file with a "Drag it to the Applications folder" installer.
+# Package the software into a DMG file with a "Drag the app bundle to the Applications folder" link.
 set(CPACK_GENERATOR DragNDrop)
 set(CPACK_DMG_CREATE_APPLICATIONS_LINK ON)
 set(CPACK_DMG_VOLUME_NAME "${OSC_LONG_APPNAME} ${PROJECT_VERSION}")
 
-# Install the `osc` bundle target (includes executable and plist).
-#
-# It's assumed that the bundle already contains all necessary
-# runtime resources (e.g. images, configuration files) because
-# Apple treats the entire application container (`.app`) as the
-# resulting binary.
-install(TARGETS osc BUNDLE DESTINATION .)
-
-# Ensure the installer is named `opensimcreator-${version}-macos-${arch}.dmg`.
+# Ensure the package is named `opensimcreator-${version}-macos-${arch}.dmg`.
 if(CMAKE_OSX_ARCHITECTURES)
     if(CMAKE_OSX_ARCHITECTURES STREQUAL "x86_64")
         set(CPACK_SYSTEM_NAME "macos-amd64")
