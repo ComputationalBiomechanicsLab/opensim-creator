8c3ee9bd7573cb24d84452de81d6bab9228fbf98
Adam Kewley <contact@adamkewley.com>
2026-01-09 08:23:35 +0100

    Remove venv logic from scripts/ci_build_docs.sh

--- scripts/ci_build_docs.sh
+++ scripts/ci_build_docs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-python -m venv .venv && source .venv/Scripts/activate && pip install -r requirements/all_requirements.txt && sphinx-build docs/source build/docs
+sphinx-build docs/source build/docs
+
