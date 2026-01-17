9dd54b1c67cf842a5f4905ceb531249114b1d2ca
Adam Kewley <contact@adamkewley.com>
2026-01-17 14:13:43 +0100

    Update oscar

--- third_party/opynsim/libopynsim/tests/meta.c2tests.cpp
+++
@@ -1,6 +0,0 @@
-#include <catch2/catch_all.hpp>
-
-TEST_CASE("Catch2 configures, builds, and runs")
-{
-    // This should work (it just exercises the build system, not opynsim)
-}
---
+++ third_party/opynsim/libopynsim/tests/meta.tests.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(meta, gtest_configures_builds_and_runs)
+{
+    // This should work (it just exercises the build system, not opynsim)
+}
