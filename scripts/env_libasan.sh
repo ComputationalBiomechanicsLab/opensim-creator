7946fceed87708103e6d9c026daa7965a1c7482c
Adam Kewley <contact@adamkewley.com>
2026-02-16 17:07:07 +0100

    Update OPynSim and add UBSAN to error checks

--- scripts/env_libasan.sh
+++ scripts/env_libasan.sh
@@ -1,6 +1,9 @@
 # Setup libASAN suitable for simbody/OpenSim/Creator
 export ASAN_OPTIONS="abort_on_error=1:strict_string_checks=true:malloc_context_size=30:check_initialization_order=true:detect_stack_use_after_return=true:strict_init_order=true:detect_invalid_pointer_pairs=2"
 
+# Setup libUBSAN suitable for simbody/OpenSim/Creator
+export UBSAN_OPTIONS="abort_on_error=1:print_stacktrace=1:print_summary=1:suppressions=${PWD}/scripts/suppressions_ubsan.supp"
+
 # Add suppressions to libLSAN (usually, they're just OS suppressions)
 export LSAN_OPTIONS="suppressions=${PWD}/scripts/suppressions_lsan.supp"
 
